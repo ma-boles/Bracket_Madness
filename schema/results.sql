43e12ece67820471cbb951eb3f672e2a72e4c486
ma-boles <ma_boles@hotmail.com>
2025-03-11 18:26:44 -0700

    git commit -m "added columns espn_game_id, seed_team1, seed_team2"

--- schema/results.sql
+++ schema/results.sql
@@ -1,9 +1,13 @@
 CREATE TABLE results(
     game_id INT AUTO_INCREMENT PRIMARY KEY,
+    espn_game_id INT,
     round VARCHAR(20) NOT NULL,
+    seed_team1 INT NOT NULL,
     team1 VARCHAR(50) NOT NULL,
-    team2 VARCHAR(50) NOT NULL,
     score1 INT NOT NULL,
+    seed_team2 INT NOT NULL,
+    team2 VARCHAR(50) NOT NULL,
     score2 INT NOT NULL,
-    winner VACHAR(50) NOT NULL
+    actual_winner VARCHAR(50) NOT NULL,
+    next_game_id INT
 );
