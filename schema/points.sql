f6ec63977a3d0403035f31c465d2e7d6b42a29b1
ma-boles <ma_boles@hotmail.com>
2025-03-11 18:26:06 -0700

    git commit -m "added data type to user_id"

--- schema/points.sql
+++ schema/points.sql
@@ -1,6 +1,6 @@
 CREATE TABLE points(
     id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id NOT NULL,
+    user_id INT NOT NULL,
     total_points INT DEFAULT 0,
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
