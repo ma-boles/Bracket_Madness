35e00caf75d0621d2c92ec6710700850ad0e98ac
ma-boles <ma_boles@hotmail.com>
2025-03-11 18:25:20 -0700

    git commit -m "updated column names and order"

--- schema/picks.sql
+++ schema/picks.sql
@@ -1,9 +1,9 @@
 CREATE TABLE picks(
     id INT AUTO_INCREMENT PRIMARY KEY,
-    game_id INT,
     user_id INT NOT NULL,
+    game_id INT,
     round VARCHAR(20) NOT NULL,
-    winner VARCHAR(50) NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE,
+    predicted_winner VARCHAR(50) NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
     FOREIGN KEY (game_id) REFERENCES results(game_id)
 );
