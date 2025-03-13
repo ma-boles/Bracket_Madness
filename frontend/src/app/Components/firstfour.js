e4b0e1f7e5a84ead93fed217dc437ed0765c7c4d
ma-boles <ma_boles@hotmail.com>
2025-03-12 18:55:43 -0700

    git commit -m "initial commit"

---
+++ frontend/src/app/Components/firstfour.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+export default function FirstFour() {
+    return(
+        <>
+            <div className="round">
+                <div className="matchup bg-pink-500">
+                    <div>team 1</div>
+                    <div>team 2</div>
+                </div>
+                <div className="matchup bg-yellow-500">
+                    <div>team 3</div>
+                    <div>team 4</div>
+                </div>
+                <div className="matchup bg-pink-500">
+                    <div>team 5</div>
+                    <div>team 6</div>
+                </div>
+                <div className="matchup bg-yellow-500">
+                    <div>team 7</div>
+                    <div>team 8</div>
+                </div>
+            </div>
+        </>
+    )
+}
