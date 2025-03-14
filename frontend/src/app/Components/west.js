05f60eac58b03930829d946766e52c25d7e615c3
ma-boles <ma_boles@hotmail.com>
2025-03-13 18:06:55 -0700

    git commit -m "updated stylings"

--- frontend/src/app/Components/west.js
+++ frontend/src/app/Components/west.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function West() {
     return(
         <>
-            <div className="west region bg-red-600">
+            <div className="west region">
                 <div className="rounds">
                     <div className="round first-round">
                         <div className="matchup bg-pink-500">
@@ -30,7 +30,7 @@ export default function West() {
                             <div className="team">seed 3</div>
                             <div className="team">seed 14</div>
                         </div>
-                        <div className="matchup bg-blue-600 border-r-indigo-500">
+                        <div className="matchup bg-indigo-500">
                             <div className="team">seed 7</div>
                             <div className="team">seed 10</div>
                         </div>
@@ -40,15 +40,15 @@ export default function West() {
                         </div>
                     </div>
                     <div className="round second-round">
-                        <div className="matchup2 mb-12 bg-green-600 border-r border-black justify-between">
+                        <div className="matchup2 mb-8 bg-green-600">
                             <div className="team">winner 1</div>
                             <div className="team">winner 2</div>
                         </div>
-                        <div className="matchup2 mb-12 border-r-indigo-500">
+                        <div className="matchup2 mb-8 bg-indigo-500">
                             <div className="team">winner 3</div>
                             <div className="team">winner 4</div>
                         </div>
-                        <div className="matchup2 mb-14 bg-lime-600 border-r-2 border-black">
+                        <div className="matchup2 mb-14 bg-lime-600">
                             <div className="team">winner 5</div>
                             <div className="team">winner 6</div>
                         </div>
@@ -58,11 +58,11 @@ export default function West() {
                         </div>
                     </div>
                     <div className="round sweet-16">
-                        <div className="matchup3 bg-pink-500">
+                        <div className="matchup3  bg-blue-500">
                             <div className="team">winner 9</div>
                             <div className="team">winner 10</div>
                         </div>
-                        <div className="matchup3 bg-yellow-500">
+                        <div className="matchup3-last bg-yellow-500">
                             <div className="team">winner 11</div>
                             <div className="team">winner 12</div>
                         </div>
@@ -74,7 +74,7 @@ export default function West() {
                         </div>
                     </div>
                     <div className="round final4">
-                        <div className="mt-110 matchup bg-teal-500 ">
+                        <div className="mt-100 matchup bg-teal-500 ">
                             <div className="team">winner 15</div>
                         </div>
                     </div>
