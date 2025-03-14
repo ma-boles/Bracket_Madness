f20a93f15f4210098e031f4c4ea1e08083aeb031
ma-boles <ma_boles@hotmail.com>
2025-03-13 18:05:54 -0700

    git commit -m "updated input styles"

--- frontend/src/app/Components/championship.js
+++ frontend/src/app/Components/championship.js
@@ -3,22 +3,25 @@ import React from "react";
 export default function Championship () {
     return (
         <>
-        <div className="bg-blue-600 p-4">
-            <div className="flex gap-6">
-                <div className="flex flex-col items-center">
-                <p className="text-white mb-2">Semifinal</p>
-                <input className="bg-white border-2 border-black p-2 rounded-md" />
-                </div>
-                <div className="flex flex-col items-center">
-                <p className="text-white mb-2">Champion</p>
-                <input className="bg-white border-2 border-black p-2 rounded-md" />
-                </div>
-                <div className="flex flex-col items-center">
-                <p className="text-white mb-2">Semifinal</p>
-                <input className="bg-white border-2 border-black p-2 rounded-md" />
-                </div>
+            <div className="round">
+                    {/* <h1 className="py-2 text-center text-2xl "></h1> */}
+                    <div className="flex gap-6 p-4 bg-blue-600 rounded-md">
+                        <div className="my-auto flex flex-col items-center">
+                            <input className="w-36 bg-white border-2 border-black p-2 rounded-md text-black" />
+                            {/* <input className="bg-white border-2 border-black p-2 rounded-md" />
+                            <p>Championship</p> */}
+                        </div>
+                        <div className="p-2 flex flex-col items-center">
+                            <input className="bg-white border-2 border-black p-2 rounded-md text-black text-xl" />
+                            <h1 className="text-2xl">National Champion</h1>
+                        </div>
+                        <div className="my-auto flex flex-col items-center">
+                            <input className="w-36 bg-white border-2 border-black p-2 rounded-md text-black" />
+                            {/* <input className="bg-white border-2 border-black p-2 rounded-md" />
+                            <p>Championship</p> */}
+                        </div>
+                    </div>
             </div>
-        </div>
         </>
     )
 }
--- frontend/src/app/Components/firstfour.js
+++ frontend/src/app/Components/firstfour.js
@@ -3,23 +3,26 @@ import React from "react";
 export default function FirstFour() {
     return(
         <>
-            <div className="round">
-                <div className="matchup bg-pink-500">
-                    <div>team 1</div>
-                    <div>team 2</div>
-                </div>
-                <div className="matchup bg-yellow-500">
-                    <div>team 3</div>
-                    <div>team 4</div>
-                </div>
-                <div className="matchup bg-pink-500">
-                    <div>team 5</div>
-                    <div>team 6</div>
-                </div>
-                <div className="matchup bg-yellow-500">
-                    <div>team 7</div>
-                    <div>team 8</div>
-                </div>
+            <div className="round pt-4">
+                <h1 className="py-2 text-center text-2xl">First Four</h1>
+                    <div className="flex gap-6 p-4 bg-blue-600 rounded-md">
+                        <div className="flex flex-col items-center">
+                            <input className="w-36 bg-white border-2 border-black p-2 rounded-md text-black" />
+                            <input className="w-36 bg-white border-2 border-black p-2 rounded-md text-black" />
+                        </div>
+                        <div className="flex flex-col items-center">
+                            <input className="w-36 bg-white border-2 border-black p-2 rounded-md text-black" />
+                            <input className="w-36 bg-white border-2 border-black p-2 rounded-md text-black" />
+                        </div>
+                        <div className="flex flex-col items-center">
+                            <input className="w-36 bg-white border-2 border-black p-2 rounded-md text-black" />
+                            <input className="w-36 bg-white border-2 border-black p-2 rounded-md text-black" />
+                        </div>
+                        <div className="flex flex-col items-center">
+                            <input className="w-36 bg-white border-2 border-black p-2 rounded-md text-black" />
+                            <input className="w-36 bg-white border-2 border-black p-2 rounded-md text-black" />
+                        </div>
+                    </div>
             </div>
         </>
     )
