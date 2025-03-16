a33ec07b23b7a167055b1c3014727cd61fbcc3d1
ma-boles <ma_boles@hotmail.com>
2025-03-15 17:59:08 -0700

    git commit -m "initial commit"

---
+++ frontend/src/context/BracketContext.js
@@ -0,0 +1,55 @@
+import { createContext, useContext, useState  } from "react";
+
+const BracketContext = createContext();
+
+export const BracketProvider = ({ children }) => {
+    const [ userPick, setUserPick ] = useState({ 
+        west: {},
+        east: {},
+        south: {},
+        midwest: {},
+        firstfour: {},
+        championship: {},
+    });
+
+    const handlePick = (region, game_id, team) => {
+        setUserPick((prevPicks) => ({
+            ...prevPicks,
+            [region]: {
+                ...(prevPicks[region] || {}),
+                [game_id]: team,
+            },
+        }));
+    };
+
+    const submitBracket = async () => {
+        try{
+            const response = await fetch('/api/submit-picks', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(userPicks),
+            });
+
+            if(!response.ok) {
+                throw new Error('Failed to submit picks');
+            } 
+            console.log('Bracket successfully submitted');
+        } catch (error) {
+            console.error('Submission error:', error);
+        }
+    };
+
+    return(
+        <BracketContext.Provider value={{
+            userPicks,
+            handlePick,
+            submitBracket
+        }}> 
+        {children}
+        </BracketContext.Provider>
+    );
+};
+
+export const useBracket = () => useContext(BracketContext);
