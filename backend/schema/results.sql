59fea32c0cc3d31c77985e068d3abf939170ece8
ma-boles <ma_boles@hotmail.com>
2025-03-15 17:58:10 -0700

    git commit -m "updated columns"

--- backend/schema/results.sql
+++ backend/schema/results.sql
@@ -2,12 +2,13 @@ CREATE TABLE results(
     game_id INT AUTO_INCREMENT PRIMARY KEY,
     espn_game_id INT,
     round VARCHAR(20) NOT NULL,
-    seed_team1 INT NOT NULL,
-    team1 VARCHAR(50) NOT NULL,
-    score1 INT NOT NULL,
-    seed_team2 INT NOT NULL,
-    team2 VARCHAR(50) NOT NULL,
-    score2 INT NOT NULL,
-    actual_winner VARCHAR(50) NOT NULL,
-    next_game_id INT
+    seed_team_a INT,
+    team_a VARCHAR(50),
+    score_a INT,
+    seed_team_b INT,
+    team_b VARCHAR(50),
+    score_b INT,
+    actual_winner VARCHAR(50),
+    next_game_id INT,
+    next_slot_id VARCHAR(10)
 );
