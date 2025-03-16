1db2fcb261912ed0db6f5f1b4809a680c09314c0
ma-boles <ma_boles@hotmail.com>
2025-03-16 10:58:11 -0700

    git commit -m "updated component names"

--- frontend/src/app/Profile/page.js
+++ frontend/src/app/Profile/page.js
@@ -1,11 +1,12 @@
 import React from "react";
-import West from "../Components/west";
-import East from "../Components/east";
-import South from "../Components/south";
-import Midwest from "../Components/midwest";
-import NavBar from "../Components/NavBar";
-import FirstFour from "../Components/firstfour";
-import Championship from "../Components/championship";
+import WestPick from "../../Components/westpick";
+import EastPick from "../../Components/eastpick";
+import SouthPick from "../../Components/southpick";
+import MidwestPick from "../../Components/midwestpick";
+import NavBar from "../../Components/NavBar";
+import FirstFourPick from "../../Components/firstfourpick";
+import ChampionshipPick from "../../Components/championshippick";
+
 
 export default function Profile() {
     return(
@@ -15,17 +16,17 @@ export default function Profile() {
         </nav>
         <div className="bg-slate-800">
             <div className="w-full">
-                <FirstFour />
+                <FirstFourPick />
             </div>
             <div>
                 <div className="flex">
-                    <West />
-                    <Midwest />
+                    <WestPick />
+                    <MidwestPick />
                 </div>
             <Championship />
                 <div className="flex">
-                    <East />
-                    <South />
+                    <EastPick />
+                    <SouthPick />
                 </div>
             </div>
         </div>
