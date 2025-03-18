69afc2fb74bc262165b626fe5aa2eff4c490bfe7
ma-boles <ma_boles@hotmail.com>
2025-03-17 23:04:15 -0700

    git commit -m "inserted game data"

---
+++ frontend/src/sql/schema/data/championship.sql
@@ -0,0 +1,4 @@
+INSERT INTO results (game_id, espn_game_id, round, team_a_id, team_a_score, team_b_id, team_b_score, winner_id, next_game_id, next_slot_id)
+VALUES 
+(100000, NULL, 'Championship', NULL, NULL, NULL, NULL, NULL, NULL, 'team_a_id');
+
---
+++ frontend/src/sql/schema/data/finalfour.sql
@@ -0,0 +1,5 @@
+INSERT INTO results (game_id, espn_game_id, round, team_a_id, team_a_score, team_b_id, team_b_score, winner_id, next_game_id, next_slot_id)
+VALUES 
+(9001, NULL, 'Final Four', NULL, NULL, NULL, NULL, NULL, NULL, 'team_a_id'),
+
+(9002, NULL, 'Final Four', NULL, NULL, NULL, NULL, NULL, NULL, 'team_b_id');
