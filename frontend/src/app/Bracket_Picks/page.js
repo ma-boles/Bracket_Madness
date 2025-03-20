66474ab9a0ac07d42a6d48324d6d0b755078e9bd
ma-boles <ma_boles@hotmail.com>
2025-03-19 18:51:25 -0700

    git commit -m "updated with renamed region components and submit button"

--- frontend/src/app/Bracket_Picks/page.js
+++ frontend/src/app/Bracket_Picks/page.js
@@ -1,13 +1,17 @@
+'use client'
 import React from "react";
 import NavBar from "../../Components/NavBar";
-import West from "../../Components/spokane1_pick";
-import East from "../../Components/spokane4_pick";
-import South from "../../Components/birmingham3_pick";
-import Midwest from "../../Components/birmingham2_pick";
 import FirstFour from "../../Components/firstfourpick";
-import Championship from "../../Components/championshippick";
+import ChampionshipPick from "../../Components/championshippick";
+import Spokane1_Pick from "../../Components/spokane1_pick";
+import Birmingham2_Pick from "../../Components/birmingham2_pick";
+import Birmingham3_Pick from "../../Components/birmingham3_pick";
+import Spokane4_Pick from "../../Components/spokane4_pick";
+import { useBracket } from "@/context/BracketContext";
 
 export default function Bracket_Picks() {
+    const { submitBracket } = useBracket();
+
     return(
         <>
             <nav>
@@ -19,15 +23,21 @@ export default function Bracket_Picks() {
                 </div>
                 <div>
                 <div className="flex">
-                    <West />
-                    <Midwest />
+                    <Spokane1_Pick />
+                    <Birmingham2_Pick />
                 </div>
-                    <Championship />
+                    <ChampionshipPick />
                 <div className="flex">
-                    <East />
-                    <South />
+                    <Spokane4_Pick />
+                    <Birmingham3_Pick />
                 </div>
                 </div>
+                <div className="flex justify-center items-center">
+                <button className="mb-6 rounded-lg border border-solid bg-blue-600 border-white/[0.8] transition-colors flex items-center justify-center hover:bg-white hover:text-black hover:border-transparent font-medium w-1/3 h-12 mx-2 cursor-pointer"
+                    onClick={submitBracket}>
+                    Submit
+                </button>
+            </div>
             </div>
         </>
     )
