26b5b5ca0808df7ca04878db461b17fbf69b709c
ma-boles <ma_boles@hotmail.com>
2025-03-20 18:28:07 -0700

    git commit -m "removed round column and added unique key"

--- frontend/src/sql/schema/picks.sql
+++ frontend/src/sql/schema/picks.sql
@@ -1,10 +1,11 @@
 CREATE TABLE picks(
     id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT NOT NULL,
-    game_id INT,
-    round VARCHAR(20) NOT NULL,
+    game_id INT NOT NULL,
     winner_id INT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
     FOREIGN KEY (game_id) REFERENCES results(game_id),
-    FOREIGN KEY (winner_id) REFERENCES teams(team_id)
+    FOREIGN KEY (winner_id) REFERENCES teams(team_id),
+    UNIQUE KEY unique_pick (user_id, game_id)
 );
