c0bb32e6416e27d5f874de0bb45f5e231646257a
ma-boles <ma_boles@hotmail.com>
2025-03-28 22:40:24 -0700

    git commit -m "renamed file, moved to new folder"

--- frontend/src/Components/Birmingham3/Birmingham3_Input16_FF.js
+++ frontend/src/Components/Birmingham3/Birmingham3_Input16_FF.js
@@ -1,6 +1,6 @@
 import { useBracket } from '@/context/BracketContext'
 import { Combobox, ComboboxButton, ComboboxInput, ComboboxOption, ComboboxOptions } from '@headlessui/react'
-import { CheckIcon, ChevronDownIcon } from '@heroicons/react/20/solid'
+import { ChevronDownIcon } from '@heroicons/react/20/solid'
 import clsx from 'clsx'
 import { useState } from 'react'
 
@@ -10,11 +10,11 @@ const teams = [
 ]
 
 export default function Birmingham3_Input16_FF({ region, gameId }) {
-  const { handleSelection } = useBracket();
+  const { handleSelection, selectedTeam } = useBracket();
   const [query, setQuery] = useState('')
-  const [selected, setSelected] = useState(null)
 
-  filteredPeople =
+  
+  const filteredTeams =
     query === ''
       ? teams
       : teams.filter((team) => {
@@ -23,7 +23,9 @@ export default function Birmingham3_Input16_FF({ region, gameId }) {
 
   return (
     <div className="mx-auto w-33 pt-0">
-      <Combobox value={selected} onChange={handleSelection} onClose={() => setQuery('')}>
+      <Combobox value={selectedTeam} onChange={(team) => handleSelection(region, gameId, team)}
+       onClose={() => setQuery('')}>
+
         <div className="relative">
           <ComboboxInput
             className={clsx(
@@ -31,9 +33,8 @@ export default function Birmingham3_Input16_FF({ region, gameId }) {
               'focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-white/25'
             )}
             placeholder='Select...'
-            displayValue={(team) => team?.name || ''}
+            displayValue={(team) => team?.name}
             onChange={(event) => setQuery(event.target.value)}
-            onSelect={(team) => handleSelection(region, gameId, team)}
           />
           <ComboboxButton className="group absolute inset-y-0 right-0 px-2.5 cursor-pointer">
             <ChevronDownIcon className="size-4 fill-white/60 group-data-[hover]:fill-white" />
@@ -48,7 +49,7 @@ export default function Birmingham3_Input16_FF({ region, gameId }) {
             'transition duration-100 ease-in data-[leave]:data-[closed]:opacity-0'
           )}
         >
-          {filteredPeople.map((team) => (
+          {filteredTeams.map((team) => (
             <ComboboxOption
               key={team.id}
               value={team}
