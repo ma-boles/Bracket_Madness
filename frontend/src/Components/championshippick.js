46f72f24a37e9e57cf2c0b796c73eb7494e71695
ma-boles <ma_boles@hotmail.com>
2025-03-30 17:05:16 -0700

    git commit -m "updated display for semifinal teams"

--- frontend/src/Components/championshippick.js
+++ frontend/src/Components/championshippick.js
@@ -1,24 +1,33 @@
 'use client'
 import React from "react";
 import { useBracket } from "@/context/BracketContext";
-import Spokane1_Input from "./Spokane1/Spokane1_Input";
-import Birmingham2_Input from "./Birmingham2/Birmingham2_Input";
-import Birmingham3_Input from "./Birmingham3/Birmingham3_Input";
-import Spokane4_Input from "./Spokane4/Spokane4_Input";
+// import Spokane1_Input from "./Spokane1/Spokane1_Input";
+// import Birmingham2_Input from "./Birmingham2/Birmingham2_Input";
+// import Birmingham3_Input from "./Birmingham3/Birmingham3_Input";
+// import Spokane4_Input from "./Spokane4/Spokane4_Input";
 import Winner_Input_4001 from "./Championship_Input/Winner_Input_4001";
 import Winner_Input_4002 from "./Championship_Input/Winner_Input_4002";
 import Championship_Input from "./Championship_Input/Championship_Input";
 
 
 export default function ChampionshipPick () {
+    const { bracketData } = useBracket();
 
     return (
         <>
             <div className="round">
                     <div className="flex gap-6 p-4 bg-blue-600 rounded-md">
                         <div className="my-auto flex flex-col items-center">
-                            <Spokane1_Input region='finalfour' gameId={9001}/>
-                            <Spokane4_Input region='finalfour' gameId={9001}/>
+                            <div className="mx-auto w-33 pt-0">
+                                <div className="rounded-lg border-none bg-white/5 py-1.5 pr-8 pl-3 text-sm/6 text-white">
+                                    {bracketData[4001]?.winnerName || 'Spokane 1'}
+                                </div>
+                            </div>
+                            <div className="mx-auto w-33 pt-0 ">
+                                <div className="rounded-lg border-none bg-white/5 py-1.5 pr-8 pl-3 text-sm/6 text-white">
+                                    {bracketData[4004]?.winnerName || 'Spokane 4'}
+                                </div>
+                            </div>
                         </div>
 
                         <div className="p-2 flex flex-col items-center">
@@ -31,8 +40,16 @@ export default function ChampionshipPick () {
                         </div>
 
                         <div className="my-auto flex flex-col items-center">
-                            <Birmingham2_Input region='finalfour' gameId={9002}/>
-                            <Birmingham3_Input region='finalfour' gameId={9002}/>
+                            <div className="mx-auto w-33 pt-0">
+                                <div className="rounded-lg border-none bg-white/5 py-1.5 pr-8 pl-3 text-sm/6 text-white">
+                                    {bracketData[4002]?.winnerName || 'Birmingham 2'}
+                                </div>
+                            </div>
+                            <div className="mx-auto w-33 pt-0">
+                                <div className="rounded-lg border-none bg-white/5 py-1.5 pr-8 pl-3 text-sm/6 text-white">
+                                    {bracketData[4003]?.winnerName || 'Birmingham 3'}
+                                </div>
+                            </div>
                         </div>
                     </div>
             </div>
