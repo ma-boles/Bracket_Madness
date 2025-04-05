03276094ced4c0a159f96e1df0639d2bfd3755ba
ma-boles <ma_boles@hotmail.com>
2025-04-05 11:57:54 -0700

    git commit -m "wrapped code in async function"

--- frontend/src/db/db.js
+++ frontend/src/db/db.js
@@ -1,21 +1,23 @@
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 require('dotenv').config();
 
+async function connectionToDatabase () {
+    // Database connection
+    const db = await mysql.createConnection({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME
+    });
+    console.log('Connected to the database');
+    return db;
+}
 
-// Database connection
-const db = await mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-});
-
-db.connect((err) => {
-    if(err) {
-        console.log('MySQL connection error');
-    } else {
+connectionToDatabase()
+    .then((db) => {
         console.log('Connected to MySQL!');
-    }
-});
-
-module.exports = db;
+        db.end();
+    })
+    .catch((error) => {
+        console.log('MySQL connection error');
+    });
