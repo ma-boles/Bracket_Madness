c92ff2a3d1984e68d24e9fd856751fe94643bdec
ma-boles <ma_boles@hotmail.com>
2025-04-10 16:28:16 -0700

    git commit -m "updated code to successfully create  connection"

--- frontend/src/db/db.js
+++ frontend/src/db/db.js
@@ -1,23 +1,23 @@
-const mysql = require('mysql2/promise');
 require('dotenv').config();
 
+const mysql = require('mysql2/promise');
+
+
 async function connectionToDatabase () {
-    // Database connection
-    const db = await mysql.createConnection({
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME
-    });
-    console.log('Connected to the database');
-    return db;
+    try {
+        // Database connection
+        const db = await mysql.createConnection({
+            host: process.env.DB_HOST,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME
+        });
+        console.log('Connected to the database');
+        return db;
+    } catch(error) {
+        console.error('MySQL connection error:', error);
+        throw error;
+    }
 }
 
-connectionToDatabase()
-    .then((db) => {
-        console.log('Connected to MySQL!');
-        db.end();
-    })
-    .catch((error) => {
-        console.log('MySQL connection error');
-    });
+module.exports = {connectionToDatabase};
