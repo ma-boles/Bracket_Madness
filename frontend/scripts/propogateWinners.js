3eb1fbc9c1f326445c05f44b67dd8e37074bf9a7
ma-boles <ma_boles@hotmail.com>
2025-04-15 18:00:09 -0700

    git commit -m "initial commit"

---
+++ frontend/scripts/propogateWinners.js
@@ -0,0 +1,26 @@
+const { connectionToDatabase } = require("../src/db/db");
+
+
+const propogateWinners = async() => {
+    const connection = await connectionToDatabase();
+
+    try {
+        const [results] = await connection.execute(`
+            UPDATE results as r1
+            JOIN results AS r2 ON r1.next_game_id = r2.game_id
+            SET
+                r2.team_a_id = IF(r1.next_slot_id = 'team_a_id', r1.winner_id, r2.team_a_id),
+                r2.team_b_id = IF(r1.next_slot_id = 'team_b_id', r1.winner_id, r2.team_b_id) 
+            WHERE r1.winner_id IS NOT NULL
+                AND((r1.next_slot_id = 'team_a_id' AND r2.team_a_id IS NULL)
+                    OR(r1.next_slot_id = 'team_b_id' AND r2.team_b_id IS NULL));
+        `);
+        console.log('Winner propogation completed successfully');
+    } catch(error) {
+        console.error('Error propogating winners:', error);
+    } finally {
+        await connection.end();
+    }
+};
+
+module.exports = propogateWinners;
