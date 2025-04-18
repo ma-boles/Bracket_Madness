737e12942e788ada4893020d7ffa5a73aa48aea2
ma-boles <ma_boles@hotmail.com>
2025-04-17 22:52:13 -0700

    git commit -m "added seed to display, updated styles"

--- frontend/src/Components/TeamButton.js
+++ frontend/src/Components/TeamButton.js
@@ -7,12 +7,12 @@ export default function TeamButton({ region, gameId, team }) {
 
     return (
         <button 
-            className={`w-full rounded-lg border-none bg-white/5 py-1.5 pr-8 pl-3 text-sm/6 text-white ${
-                selected ? 'bg-blue-600' : 'bg-transparent hover:bg-gray-500'
+            className={`w-full border-none bg-white/5 py-1.5 pr-8 pl-3 text-md text-white cursor-pointer team truncate whitespace-nowrap overflow-hidden${
+                selected ? 'border border-white font-bold' : 'hover:bg-gray-500'
             }`}
             onClick={() => handleSelection(region, gameId, team)}
             >
-            {team.name}
-            </button>
+            {team.seed} {team.name}
+        </button>
     );
 }
