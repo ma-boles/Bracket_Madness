ba6a300f024b5bc8e675418338043061d30e14ab
ma-boles <ma_boles@hotmail.com>
2025-04-20 16:52:48 -0700

    git commit -m "added alert to log out"

--- frontend/src/app/auth/components/LogoutButton.js
+++ frontend/src/app/auth/components/LogoutButton.js
@@ -6,7 +6,9 @@ const LogoutButton = () => {
 
     const handleLogout = () => {
         logout();
+        alert('Logged Out')
     };
+    
     return(
         <button className="w-24 p-2 mx-1 h-10 bg-red-600 cursor-pointer hover:bg-white hover:text-black transition duration-300 rounded-md" onClick={handleLogout}>
             Log Out
