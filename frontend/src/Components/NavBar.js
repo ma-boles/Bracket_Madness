ca022fa42a48f4117b7cd78a968e32e8e83fdbac
ma-boles <ma_boles@hotmail.com>
2025-04-20 16:51:47 -0700

    git commit -m "updated with code with currentUser from AuthContext"

--- frontend/src/Components/NavBar.js
+++ frontend/src/Components/NavBar.js
@@ -3,17 +3,19 @@ import { useEffect, useState } from "react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import LogoutButton from "@/app/auth/components/LogoutButton";
+import { useAuth } from "@/context/AuthContext";
 
 export default function NavBar () {
     const router = useRouter();
+    const { currentUser } = useAuth();
     const [user, setUser] = useState(null);
 
-    useEffect(() => {
-        const token = localStorage.getItem('token');
-        if(token) {
-            setUser(true);
-        }
-    }, []);
+    // useEffect(() => {
+    //     const token = localStorage.getItem('token');
+    //     if(token) {
+    //         setUser(true);
+    //     }
+    // }, []);
 
     const handleBracketClick = () => {
         if(user) {
@@ -35,7 +37,7 @@ export default function NavBar () {
                         <p className="text-2xl">Bracket Results</p>
                     </Link>
                 </div>
-                {user && (
+                {currentUser && (
                     <div className="flex-1 p-2 text-black text-center cursor-pointer transition-all duration-300 hover:bg-blue-600 hover:text-white">
                         <Link href="/Bracket_Picks" className="2xl">
                             <p className="text-2xl">Bracket Picks</p>
@@ -58,7 +60,7 @@ export default function NavBar () {
                     </ol>
                 </div>
                 <div className="p-2 flex">
-                    {user ? (
+                    {currentUser ? (
                         <LogoutButton />
                     ) : (
                         <button className="w-24 p-2 mx-1 h-10 bg-green-600 cursor-pointer hover:bg-white hover:text-black transition duration-300 rounded-md" onClick={handleLogIn}>Log In</button>
