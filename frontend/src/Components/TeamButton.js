f59ef764790d1e1450b0d1b088cef4d50ae09343
ma-boles <ma_boles@hotmail.com>
2025-04-20 15:47:03 -0700

    git commit -m "added AuthContext"

--- frontend/src/Components/TeamButton.js
+++ frontend/src/Components/TeamButton.js
@@ -1,16 +1,48 @@
 'use client'
+import { useContext, useEffect, useState } from "react";
 import { useBracket } from "@/context/BracketContext"
+import Cookies from "js-cookie";
+import jwt from 'jsonwebtoken';
+import AuthContext from "@/context/AuthContext";
 
 export default function TeamButton({ region, gameId, team }) {
     const { userPicks, handleSelection } = useBracket();
-    const selected = userPicks?.[region]?.[gameId] === team.name;
+    const { currentUser } = useContext(AuthContext);
 
+    const selected = userPicks?.[region]?.[gameId] === team.name;
+    const isDisabled = !currentUser;
+
+    // useEffect(() => {
+    //     const token = localStorage.getItem('token');
+    //     if(token) {
+    //         try {
+    //             const decoded = jwt.decode('token');
+
+    //             if(decoded && decoded.userId) {
+    //                 setCurrentUserId(decoded?.userId);
+    //                 console.log('Current user ID set:', decoded.userId);
+
+    //             } else {
+    //                 ('Decoded token is invalid or userId missing.')
+    //             }
+    //         } catch(error) {
+    //             console.log('Error decoding JWT:', error);
+    //         }
+
+    //     } else {
+    //         console.log('No JWT token found, user not logged in');
+    //     }
+    // }, []);
+    
     return (
         <button 
-            className={`w-full border-none bg-white/5 py-1.5 pr-8 pl-3 text-md text-white cursor-pointer team truncate whitespace-nowrap overflow-hidden${
-                selected ? 'border border-white font-bold' : 'hover:bg-gray-500'
-            }`}
-            onClick={() => handleSelection(region, gameId, team)}
+            className={`w-full border-none bg-white/5 py-1.5 pr-8 pl-3 text-md text-white cursor-pointer team truncate whitespace-nowrap overflow-hidden
+                ${selected ? 'border border-white font-bold' : 'hover:bg-gray-500'}
+                ${isDisabled ? 'bg-gray-400 cursor-not-allowed' : 'bg-white/5'}
+                ${isDisabled ? 'pointer-events-none' : ''} 
+            `}
+            disabled={isDisabled}
+            onClick={() => !isDisabled && handleSelection(region, gameId, team)}
             >
             {team.seed} {team.name}
         </button>
