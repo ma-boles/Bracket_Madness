e7036270b45e7484ef48249752cbe911128f11b0
ma-boles <ma_boles@hotmail.com>
2025-04-21 11:37:07 -0700

    git commit -m "initial commit"

---
+++ frontend/src/sql/schema/brackets.sql
@@ -0,0 +1,7 @@
+CREATE TABLE brackets(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    bracket_name VARCHAR(255),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
