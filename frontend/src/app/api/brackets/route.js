705234eab23c7c656e57a9e1430856068b36bf60
ma-boles <ma_boles@hotmail.com>
2025-04-21 21:02:50 -0700

    git comit -m "initial commit"

---
+++ frontend/src/app/api/brackets/route.js
@@ -0,0 +1,69 @@
+import mysql from 'mysql2';
+import jwt from 'jsonwebtoken';
+import connectionToDatabase from '../src/db/db';
+import { NextResponse } from 'next/server';
+require('dotenv').config();
+
+
+// Utility function to verify JWT token
+const verifyToken = (token) => {
+
+    const secretKey = process.env.JWT_SECRET;
+
+    if(!secretKey) {
+        throw new Error('JWT secret key is not defined in the environment variables');
+    }
+
+    try {
+        const decoded = jwt.verify(token, secretKey);
+        return decoded; // Returns user info (user_id, ect)
+    } catch(error) {
+        return null;
+    }
+};
+
+export async function POST(req) {
+    let db;
+
+    try {
+        // Database connection
+        const db = await connectionToDatabase();
+        const token = req.cookies.get('token')?.value; 
+        const decodedUser = verifyToken(token); // Verify JWT token
+
+        if(!decodedUser) {
+            // If token is invalid, return 401 unauthorized response
+            return NextResponse.json({ message: 'Unauthorized. Please log in.' });
+        }
+
+        const { bracket_name } = await req.json();
+        const userId = decodedUser.userId;
+
+        // Prepare query to insert bracket data
+        const [result] = await db.execute ( `
+            INSERT INTO brackets (user_id, bracketName)
+            VALUES(?, ?)`, [userId, bracket_name || null]
+        );
+
+        const insertedId = result.insertedId;
+        let finalName = bracket_name;
+
+        // Generate default name
+        if(!bracket_name) {
+            const shortYear = new Date().getFullYear().toString().slice(-2);
+            finalName = `Challenge${shortYear}_00${insertedId}`;
+            await db.exectute(
+                `UPDATE brackets SET bracket_name = ? WHERE id = ?`,
+                [finalName, insertedId]
+            );
+        }
+
+        return NextResponse.json({ success: true, bracket_id: insertedId, bracket_name: finalName });
+
+    } catch(error) {
+        console.error('Error submitting bracket data:', error);
+        return NextResponse.json({ success: false, message: 'Internal Server Error '});
+    } finally {
+        if (db) await db.end();
+    }
+}
