511a59941354c52d448584c756460b1665ffd253
ma-boles <ma_boles@hotmail.com>
2025-04-23 15:47:37 -0700

    git commit -m "initial commit"

---
+++ frontend/src/Components/ui/Loader.js
@@ -0,0 +1,9 @@
+export default function Loader() {
+    return(
+        <>
+            <h1 className="mb-12 text-white text-2xl">Logging in...</h1>
+            <div className="loader"></div> 
+        </>
+    )
+}
+
