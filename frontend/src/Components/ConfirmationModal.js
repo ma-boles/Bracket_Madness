d810457c71e4ed25dd9d8283b6cb85f07b647984
ma-boles <ma_boles@hotmail.com>
2025-04-22 18:23:49 -0700

    git commit -m "updated styling"

--- frontend/src/Components/ConfirmationModal.js
+++ frontend/src/Components/ConfirmationModal.js
@@ -7,24 +7,24 @@ export default function ConfirmationModal({ show, onClose, onVerify, onSubmitBra
         <>
         <div className="fixed inset-0 bg-black/80 bg-opacity-50 z-40" onClick={onClose}/>
             <div className="py-4 px-28 fixed top-1/2 left-1/2 z-50 transform -translate-x-1/2 -translate-y-1/2 border-2 border-white bg-black text-center rounded-lg">
-            
+
                 {!picksValid ? (
                     <div>
-                        <h2 className="mb-4 font-semibold text-xl">Please verify your picks.</h2> 
+                        <h2 className="mb-4 font-semibold text-xl">Please verify your picks.</h2>
                         <button className="p-2 m-2 w-52 border border-white rounded-lg cursor-pointer hover:bg-purple-600 hover:border-purple-600 transition duration-300" onClick={onVerify}>Verify</button>
-                    </div>  
+                    </div>
                     ) : (
                     <div>
-                        <div>
-                            <label className="font-semibold">Bracket Name (optional):</label>
+                        <div className="my-2">
+                            <label className="font-semibold text-xl">Bracket Name (optional):</label>
                             <input
                             type="text"
                             placeholder="e.g. My Bracket 2025"
                             value={bracketName}
                             onChange={(e) => setBracketName(e.target.value)}
-                            className="p-2 my-4 w-52 bg-white/10 rounded-lg"/>
+                            className="p-2 mt-8 mx-2 w-52 bg-white/10 rounded-lg"/>
                         </div>
-                        <button className="p-2 w-52 border border-white rounded-lg cursor-pointer hover:bg-purple-600 hover:border-purple-600 transition duration-300" onClick={onSubmitBracket}>Submit</button>
+                        <button className="p-2 my-2 w-52 border border-white rounded-lg cursor-pointer hover:bg-purple-600 hover:border-purple-600 transition duration-300" onClick={onSubmitBracket}>Submit</button>
                     </div>
                 )}
 
@@ -32,4 +32,3 @@ export default function ConfirmationModal({ show, onClose, onVerify, onSubmitBra
         </>
     )
 }
-                    
