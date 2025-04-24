535aeb8366bc3b12859826a4e1952d344ee5de9a
ma-boles <ma_boles@hotmail.com>
2025-04-23 17:43:30 -0700

    git commit -m "added toast"

--- frontend/src/app/auth/components/LogoutButton.js
+++ frontend/src/app/auth/components/LogoutButton.js
@@ -1,14 +1,20 @@
 import React, { useContext } from "react";
 import AuthContext from "@/context/AuthContext";
+import toast from "react-hot-toast";
 
 const LogoutButton = () => {
     const { logout } = useContext(AuthContext);
 
     const handleLogout = () => {
         logout();
-        alert('Logged Out')
+        toast.success('Logged out successfully!', {
+            style: {
+                background: '#333',
+                color: '#fff'
+            }
+        })
     };
-    
+
     return(
         <button className="w-24 p-2 mx-1 h-10 bg-red-600 cursor-pointer hover:bg-white hover:text-black transition duration-300 rounded-md" onClick={handleLogout}>
             Log Out
