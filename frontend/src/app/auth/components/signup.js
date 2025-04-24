e9bbec37a638852f853b436fcf474f17345ca49e
ma-boles <ma_boles@hotmail.com>
2025-04-23 18:34:18 -0700

    git commit -m "added toast and loading messages"

--- frontend/src/app/auth/components/signup.js
+++ frontend/src/app/auth/components/signup.js
@@ -1,38 +1,81 @@
 'use client'
+import Loader from "@/Components/ui/Loader";
 import { useRouter } from "next/navigation";
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import AuthContext from "@/context/AuthContext";
+import toast from "react-hot-toast";
 
 export default function SignUp() {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [ isLoading, setIsLoading ] = useState(false);
+    const [ message, setMessage ] = useState('');
+    const [ logInSuccess, setLogInSuccess ] = useState(false);
+    const { logIn } = useContext(AuthContext);
     const router = useRouter();
 
     const handleSignUp = async (e) => {
         e.preventDefault();
 
-        const res = await fetch('/api/auth/signup', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ username, password }),
-        });
+        if(!username || !password) {
+            toast.error('Please enter both a username and password.', {
+                style: {
+                    background: '#333',
+                    color: '#fff',
+                    duration: 4000,
+                }
+            });
+            return;
+        }
 
-        const data = await res.json();
+        setIsLoading(true);
+        setMessage('');
 
-        if(res.ok) {
-            localStorage.setItem('token', data.token);
-            router.push('/Womens_Bracket');
-        } else {
-            console.error("Sign Up Failed", data.message);
+        try {
+            const res = await fetch('/api/auth/signup', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ username, password }),
+            });
+
+            const data = await res.json();
+
+            if(res.ok) {
+                setMessage('Redirecting...');
+                setLogInSuccess(true);
+                setTimeout(() => {
+                    logIn(data.token);
+                    // localStorage.setItem('token', data.token);
+                    router.push('/Bracket_Picks');
+                }, 1000);
+            } else {
+                setMessage('Log in failed. Please try again.');
+            }
+        } catch(error) {
+            setMessage('An unexpected error occurred.');
+            setIsLoading(false);
+        } finally {
+            setIsLoading(false);
         }
     };
 
     return (
         <>
-        <div className="p-6 bg-black rounded-md flex flex-col items-center justify-center space-y-4">
-            <h1 className="mb-12 text-white text-2xl">Sign Up</h1>
+        <div className="w-90 h-80 p-6 bg-black rounded-md flex flex-col items-center justify-center space-y-4">
+            <h1 className="mb-12 text-white text-2xl">
+                {isLoading
+                ? 'Signing up...'
+                : message || 'Sign Up'}
+            </h1>
 
+            {isLoading ? (
+                <>
+                    <Loader />
+                </>
+            ) : !logInSuccess && (
+            <>
             <div className="flex flex-col items-center space-y-2">
                 <input className="w-60 p-2 bg-[#000E14] border-1 border-white rounded-md" 
                 type="text"
@@ -50,7 +93,9 @@ export default function SignUp() {
             <button className="w-60 bg-[#f2f2f2] text-black py-2 rounded-lg hover:bg-[#0081B8] hover:text-white transition duration-300 cursor-pointer"
             onClick={handleSignUp}
             >Sign Up</button>
-            
+
+            </>
+            )}
         </div>
         </>
     )
