193140944c31506dd7f17937192a3c5a41aaea5e
ma-boles <ma_boles@hotmail.com>
2025-04-24 14:37:59 -0700

    git commit -m "added new columns bracket_id, game_id, points_awarded, round, foreign keys and unique key"

--- frontend/src/sql/schema/points.sql
+++ frontend/src/sql/schema/points.sql
@@ -1,6 +1,12 @@
 CREATE TABLE points(
     id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT NOT NULL,
-    total_points INT DEFAULT 0,
-    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+    bracket_id INT NOT NULL,
+    game_id INT NOT NULL,
+    points_awarded INT,
+    round VARCHAR(15),
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (bracket_id) REFERENCES brackets(id) ON DELETE CASCADE,
+    FOREIGN KEY (game_id) REFERENCES results(game_id) ON DELETE CASCADE,
+    UNIQUE KEY unique_bracket_game (bracket_id, game_id)
 );
