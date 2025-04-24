2fc0389bdd9d707aad1e40f22e4611c8a25dc4a7
ma-boles <ma_boles@hotmail.com>
2025-04-23 18:35:21 -0700

    git commit -m "added toast and loading messages"

--- frontend/src/app/auth/components/login.js
+++ frontend/src/app/auth/components/login.js
@@ -2,61 +2,91 @@
 import React, { useState, useContext } from "react";
 import AuthContext from "@/context/AuthContext";
 import { useRouter } from "next/navigation";
+import Loader from "@/Components/ui/Loader";
 
 
 export default function LogIn() {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] =useState('');
+    const [ username, setUsername ] = useState('');
+    const [ password, setPassword ] =useState('');
+    const [ isLoading, setIsLoading ] = useState(false);
+    const [ message, setMessage ] = useState('');
+    const [ logInSuccess, setLogInSuccess ] = useState(false);
     const { logIn } = useContext(AuthContext);
     const router = useRouter();
     
     const handleLogin = async (e) => {
         e.preventDefault();
+        setIsLoading(true);
 
-        const res = await fetch("/api/auth/login", {
-            method:'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ username, password }),
-        });
+        setMessage('');
 
-        const data = await res.json();
+        try {
+            const res = await fetch("/api/auth/login", {
+                method:'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ username, password }),
+            });
 
-        if(res.ok) {
-            console.log('Login successful:', data.message);
-            // console.log('Received token:', data.token); // Log the token to check it
+            const data = await res.json();
 
-            logIn(data.token);
-            router.push('/Bracket_Picks');
-        } else {
-            console.log('LogIn Failed', data.message);
+            if(res.ok) {
+                setMessage('Redirecting...');
+                setLogInSuccess(true);
+                setTimeout(() => {
+                    logIn(data.token);
+                    router.push('/Bracket_Picks');
+                }, 1000);
+            } else {
+                setMessage('Login failed. Please try again.');
+                setIsLoading(false);
+            } 
+        } catch(error) {
+            setMessage('An unexpeced error occurred.');
+            setIsLoading(false);
+        } finally {
+            setIsLoading(false);
         }
     };
 
     return (
         <>
-        <div className="p-6 bg-black rounded-md flex flex-col items-center justify-center space-y-4">
-            <h1 className="mb-12 text-white text-2xl">Log In</h1>
-            
-            <div className="flex flex-col items-center space-y-2">
-                <input className="w-60 p-2 bg-[#000E14] border-1 border-white rounded-md" 
-                type="text"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-                placeholder="Username" />
+        <div className="w-90 h-80 p-6 bg-black rounded-md flex flex-col items-center justify-center space-y-4">
+            <h1 className={`mb-12 text-white text-2xl ${message?.toLowerCase().includes('success') ? 'text-green-500' : 'text-white'}`}>
+                {isLoading
+                    ? 'Logging in...'
+                    : message || 'Log In'
+                    }
+                </h1>
 
-                <input className="w-60 p-2 bg-[#000E14] border-1 border-white rounded-md" 
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                placeholder="Password" />
-            </div>
+            {isLoading ? (
+                <>
+                    <Loader />
+                </>
+                ) : !logInSuccess && (
+                <>
+                <div className="flex flex-col items-center space-y-2">
+                    <input className="w-60 p-2 bg-[#000E14] border-1 border-white rounded-md" 
+                    type="text"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    placeholder="Username" />
 
-            <button className="w-60 bg-[#f2f2f2] text-black py-2 rounded-lg hover:bg-[#0081B8] hover:text-white transition duration-300 cursor-pointer"
-            onClick={handleLogin}
-            >Log In
-            </button>
+                    <input className="w-60 p-2 bg-[#000E14] border-1 border-white rounded-md" 
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    placeholder="Password" />
+                </div>
+
+                <button className="w-60 bg-[#f2f2f2] text-black py-2 rounded-lg hover:bg-[#0081B8] hover:text-white transition duration-300 cursor-pointer"
+                onClick={handleLogin}
+                >Log In
+                </button>
+                </>
+             
+             )}
         </div>
         </>
     )
