096d504f09bf29534f891c5bae23d7c32cbbb88e
ma-boles <ma_boles@hotmail.com>
2025-04-24 18:23:00 -0700

    git commit -m "updated columns"

--- frontend/src/sql/schema/points.sql
+++ frontend/src/sql/schema/points.sql
@@ -2,11 +2,11 @@ CREATE TABLE points(
     id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT NOT NULL,
     bracket_id INT NOT NULL,
-    game_id INT NOT NULL,
-    points_awarded INT,
     round VARCHAR(15),
+    game_id INT NOT NULL,
+    awarded_points INT NOT NULL DEFAULT 0,
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
     FOREIGN KEY (bracket_id) REFERENCES brackets(id) ON DELETE CASCADE,
     FOREIGN KEY (game_id) REFERENCES results(game_id) ON DELETE CASCADE,
-    UNIQUE KEY unique_bracket_game (bracket_id, game_id)
+    UNIQUE KEY unique_bracket_game (user_id, bracket_id, game_id)
 );
