257009f3f271861a3650b0ad981ade5c4b0912d7
ma-boles <ma_boles@hotmail.com>
2025-04-30 16:18:08 -0700

    git commit -m "added rank column"

--- frontend/src/sql/schema/brackets.sql
+++ frontend/src/sql/schema/brackets.sql
@@ -2,6 +2,14 @@ CREATE TABLE brackets(
     id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT NOT NULL,
     bracket_name VARCHAR(255),
+    first_round_points INT NOT NULL DEFAULT 0,
+    second_round_points INT NOT NULL DEFAULT 0,
+    sweet16_points INT NOT NULL DEFAULT 0,
+    elite8_points INT NOT NULL DEFAULT 0,
+    final4_points INT NOT NULL DEFAULT 0,
+    championship_points INT NOT NULL DEFAULT 0,
+    total_points INT NOT NULL DEFAULT 0,
+    rank INT NOT NULL DEFAULT 0,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
