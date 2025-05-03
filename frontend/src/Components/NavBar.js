4282037ce1f69dbe7538df2ebef2b288dcde791b
ma-boles <ma_boles@hotmail.com>
2025-05-02 18:14:54 -0700

    git commit -m "updated styling"

--- frontend/src/Components/NavBar.js
+++ frontend/src/Components/NavBar.js
@@ -4,60 +4,86 @@ import Link from "next/link";
 import { useRouter } from "next/navigation";
 import LogoutButton from "@/app/auth/components/LogoutButton";
 import { useAuth } from "@/context/AuthContext";
+import Image from "next/image";
+import toast from "react-hot-toast";
 
 export default function NavBar () {
     const router = useRouter();
     const { currentUser } = useAuth();
 
-    const handleBracketClick = () => {
-        if(currentUser) {
-            router.push("/Bracket_Picks");
-        } else {
-            router.push("/auth");
-        }
+    // const handleBracketClick = () => {
+    //     if(currentUser) {
+    //         router.push("/Bracket_Picks");
+    //     } else {
+    //         router.push("/auth");
+    //     }
+    // };
+
+    const submitAlert = () => {
+        if(!currentUser) {
+            toast.error('To submit a bracket, please Log In.',{
+                style: {
+                    background: '#333',
+                    color: '#fff',
+                    duration: 4000,
+            }})
+        } 
     };
 
     const handleLogIn = () => {
         router.push('/auth');
-    }
+    };
 
     return(
         <>
-            <div className="flex w-full bg-white/85">
-                <div className="flex-1 p-2 text-center text-black hover:bg-blue-600 hover:text-white transition 300s">
-                    <Link href="/Dashboard" className="2xl">
-                        <p className="text-2xl">Dashboard</p>
-                    </Link>
+            <div className="flex bg-white/5 mt-2 mx-12 rounded-xl justify-center items-center" >
+                <div className="w-1/5 h-15 flex justify-center items-center">
+                    <Image
+                        src="/BM_logo4.jpg"
+                        alt="logo"
+                        width={150}
+                        height={20}
+                        className="object-cover opacity-60"
+                    />
                 </div>
-                <div className="flex-1 p-2 text-center text-black hover:bg-blue-600 hover:text-white transition 300s">
-                    <Link href="/Womens_Bracket" className="2xl">
-                        <p className="text-2xl">Bracket Results</p>
-                    </Link>
-                </div>
-                {currentUser && (
-                    <div className="flex-1 p-2 text-black text-center cursor-pointer transition-all duration-300 hover:bg-blue-600 hover:text-white">
-                        <Link href="/Bracket_Picks" className="2xl">
-                            <p className="text-2xl">Bracket Picks</p>
+                
+                {/* Nav Buttons Section*/}
+                <div className="flex flex-grow text-lg"> 
+                    {currentUser && (
+                        <div className="flex-1 p-3 text-center cursor-pointer transition-all duration-300 hover:bg-white/20 rounded-lg">
+                            <Link href="/Dashboard">
+                                <p>Dashboard</p>
+                            </Link>
+                        </div>
+                    )}
+                    <div className="flex-1 p-3 text-center hover:bg-white/20 transition 300s rounded-lg">
+                        <Link href="/Womens_Bracket">
+                            <p>Results</p>
                         </Link>
                     </div>
-                )}
+                        <div className="flex-1 p-3 text-center cursor-pointer transition-all duration-300 hover:bg-white/20 rounded-lg">
+                            <Link href="/Bracket_Picks" onClick={submitAlert}>
+                                <p>Submit</p>
+                            </Link>
+                        </div>
+                    
 
-                
-                <div className="flex-1 p-2 bg-blue-600 text-center cursor-pointer transition-all duration-300 hover:bg-blue-500">
-                     <ol className="px-1">
-                    <li> 
-                        <details>
-                        <summary className="text-2xl">Leader Board</summary>
-                        <ul className="bg-base-100 p-2">
-                            <li>user1</li>
-                            <li>user2</li>
-                            <li>user3</li>
-                        </ul>
-                        </details>
-                    </li>
-                    </ol>
+                    <div className="flex-1 p-3 text-center cursor-pointer transition-all duration-300 hover:bg-white/20 rounded-lg">
+                        <ol className="px-1">
+                        <li> 
+                            <details>
+                            <summary>Leader Board</summary>
+                            <ul className="bg-base-100 p-2">
+                                <li>user1</li>
+                                <li>user2</li>
+                                <li>user3</li>
+                            </ul>
+                            </details>
+                        </li>
+                        </ol>
+                    </div>
                 </div>
-                <div className="p-2 flex">
+                <div className="flex w-1/6 p-2 justify-end items-center">
                     {currentUser ? (
                         <LogoutButton />
                     ) : (
