a5bb6e98fc5b5c06c0cd3c4ee54185278598513e
ma-boles <ma_boles@hotmail.com>
2025-05-02 18:17:46 -0700

    git commit -m "updated header styles"

--- frontend/src/app/Dashboard/page.js
+++ frontend/src/app/Dashboard/page.js
@@ -16,7 +16,7 @@ export default function Dashboard() {
             <NavBar />
         </nav>
 
-        <div className="flex justify-between overflow-hidden m-2 h-40 bg-gradient-to-r from-blue-600 via-transparent to-black rounded-lg">
+        <div className="flex justify-between overflow-hidden mx-2 my-8 h-50 bg-gradient-to-r from-blue-600 via-black to-black rounded-lg">
             <h1 className="p-6 text-4xl font-bold"> Hello, username</h1>
             <Image
                 src="/BracketMadness_background.jpg"
