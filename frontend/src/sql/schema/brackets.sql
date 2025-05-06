25118510021103eb0c21665dc333f12e5cb5d34a
ma-boles <ma_boles@hotmail.com>
2025-05-05 18:34:19 -0700

    git commit -m "added new column first_four_points"

--- frontend/src/sql/schema/brackets.sql
+++ frontend/src/sql/schema/brackets.sql
@@ -2,6 +2,7 @@ CREATE TABLE brackets(
     id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT NOT NULL,
     bracket_name VARCHAR(255),
+    first_four_points INT NOT NULL DEFAULT 0,
     first_round_points INT NOT NULL DEFAULT 0,
     second_round_points INT NOT NULL DEFAULT 0,
     sweet16_points INT NOT NULL DEFAULT 0,
