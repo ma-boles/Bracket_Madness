a6ef9caf635e0bd2044fc6cad139ab66420167ee
ma-boles <ma_boles@hotmail.com>
2025-05-05 18:47:26 -0700

    git commit -m "added code to fetch bracket data"

--- frontend/src/app/Dashboard/page.js
+++ frontend/src/app/Dashboard/page.js
@@ -1,51 +1,81 @@
+'use client'
 import NavBar from "@/Components/NavBar";
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Image from "next/image";
 import BracketCard from "@/Components/Bracket/BracketCard";
+import AuthContext from "@/context/AuthContext";
+import axios from "axios";
 
 export default function Dashboard() {
+    const [ bracketsCardData, setBracketsCardData ] = useState([]);
+    const { currentUser } = useContext(AuthContext);
+    const userId = currentUser?.userId;
 
-    const bracketsCardData = [
-        { name: 'First Bracket 1', id: 6, total_points: 40, ranking: 5},
-        { name: 'Bracket #2', id: 7, total_points: 40, ranking: 5},
-    ];
 
+    useEffect(() => {
+        const fetchBrackets = async () => {
+            try {
+                const res = await axios.get(`/api/brackets/user`);
+                setBracketsCardData(res.data);
+            } catch(error) {
+                console.error('Failed to fetch bracket data:', error);
+            }
+        };
+        if(userId) {
+            fetchBrackets();
+        }
+    }, [userId]);
+
+    const bracketCards = bracketsCardData.map((item, index) => {
+        const bracketInfoData = [
+            { round: 'First Four', round_rank: 0, round_points: item.first_four_points},
+            { round: '1st Round', round_rank: 0, round_points: item.first_round_points },
+            { round: '2nd Rd', round_rank: 0, round_points: item.second_round_points },
+            { round: 'Sweet 16', round_rank: 0, round_points: item.sweet16_points },
+            { round: 'Elite 8', round_rank: 0, round_points: item.elite8_points },
+            { round: 'Final Four', round_rank: 0, round_points: item.final4_points },
+            { round: 'Championship', round_rank: 0, round_points: item.championship_points },
+        ];
+
+        return (
+            <BracketCard 
+                key={index}
+                name={item.bracket_name}
+                total_points={item.total_points}
+                ranking={item.ranking}
+                bracketInfoData={bracketInfoData}
+                />
+        );
+    });
+    
     return(
         <>
-        <nav>
-            <NavBar />
-        </nav>
+            <nav>
+                <NavBar />
+            </nav>
 
-        <div className="flex justify-between overflow-hidden mx-2 my-8 h-50 bg-gradient-to-r from-blue-600 via-black to-black rounded-lg">
-            <h1 className="p-6 text-4xl font-bold"> Hello, username</h1>
-            <Image
-                src="/BracketMadness_background.jpg"
-                alt="header"
-                width={570}
-                height={330}
-                className="py-2 object-cover opacity-40"
-            />
-        </div>
-
-        <div className="m-2 border border-white bg-white/5 rounded-lg">
-            <h1 className="m-2 py-2 text-3xl font-semibold">My Brackets</h1>
-            <div className="flex mx-2 py-2 bg-white/15 rounded-t-lg">
-                <p className="w-1/4 text-center font-semibold">Bracket Name</p>
-                <p className="w-1/4 text-center font-semibold">Bracket ID</p>
-                <p className="w-1/4 text-center font-semibold">Total Points</p>
-                <p className="w-1/4 text-center font-semibold">Ranking</p>
+            <div className="flex justify-between overflow-hidden mx-2 my-8 h-50 bg-gradient-to-r from-blue-600 via-black to-black rounded-lg">
+                <h1 className="p-6 text-4xl font-bold"> Hello, username</h1>
+                <Image
+                    src="/BracketMadness_background.jpg"
+                    alt="header"
+                    width={570}
+                    height={330}
+                    className="py-2 object-cover opacity-40"
+                />
             </div>
 
-            {bracketsCardData.map((item, index) => (
-                    <BracketCard 
-                        key={index}
-                        name={item.name}
-                        id ={item.id}
-                        total_points={item.total_points}
-                        ranking={item.ranking}
-                        />
-                    ))}
-        </div>
+            <div className="m-2 border border-white bg-white/5 rounded-lg">
+                <h1 className="m-2 py-2 text-3xl font-semibold">My Brackets</h1>
+                <div className="flex mx-2 py-2 bg-white/15 rounded-t-lg">
+                    <p className="w-3/5 text-center font-semibold">Bracket Name</p>
+                    <p className="w-1/5 text-center font-semibold">Total Points</p>
+                    <p className="w-1/5 text-center font-semibold">Ranking</p>
+                </div>
+
+                {bracketCards}
+
+            </div>
         </>
     )
 }
