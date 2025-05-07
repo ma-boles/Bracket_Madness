a7078c204205adefe69f290c097bd2fbae45ac45
ma-boles <ma_boles@hotmail.com>
2025-05-06 22:31:32 -0700

    git commit -m "updated styling, fixed team_name prop"

--- frontend/src/Components/Results/ChampionshipResult.js
+++ frontend/src/Components/Results/ChampionshipResult.js
@@ -3,9 +3,9 @@ import React from "react";
 export default function ChampionshipResult({ region, gameId, team }) {
     return (
         <div 
-            className={`w-50 text-xl font-bold text-white truncate whitespace-nowrap overflow-hidden`}
+            className={`w-50 text-xl font-bold text-white text-center truncate whitespace-nowrap overflow-hidden`}
             >
-            {team.seed} {team.name}
+            {team.seed} {team.team_name}
         </div>
     );
 }
