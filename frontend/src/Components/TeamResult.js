434e4900ccb88e2ee28b8011d3af60b23f4b7275
ma-boles <ma_boles@hotmail.com>
2025-05-06 22:30:44 -0700

    git commit -m "updated styling, added logging"

--- frontend/src/Components/TeamResult.js
+++ frontend/src/Components/TeamResult.js
@@ -1,13 +1,17 @@
 import React from "react";
 
 export default function TeamResult({ region, gameId, team }) {
+    if (!team) {
+        console.warn(`Missing team for region ${region} and game ${gameId}`);
+        return null;
+    }    
+
     return (
         <>
-            <div 
-            className={`w-full border-none bg-white/5 py-1.5 pr-8 pl-3 text-md text-white team truncate whitespace-nowrap overflow-hidden
-            `}
+        <div 
+            className={`w-full border-none bg-white/5 pl-3 team text-md text-white truncate whitespace-nowrap overflow-hidden`}
             >
-            {team.seed} {team.name}
+            {team.seed} {team.team_name}
         </div>
         </>
     )
