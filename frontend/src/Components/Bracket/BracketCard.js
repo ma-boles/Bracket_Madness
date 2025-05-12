c192233c0b5b805eed900a10c6bba1c11bb3da9c
ma-boles <ma_boles@hotmail.com>
2025-05-12 15:04:25 -0700

    git commit -m "added View and Details buttons"

--- frontend/src/Components/Bracket/BracketCard.js
+++ frontend/src/Components/Bracket/BracketCard.js
@@ -16,33 +16,42 @@ export default function BracketCard({ name, bracketId, total_points, ranking, br
     return(
         <>
         <div className="m-2 bg-purple-900/10 rounded-lg">
-            <div className="flex mt-2 h-20 bg-purple-900 cursor-pointer hover:border-2 rounded-lg" 
-                onClick={handleToggle}>
-
-                <div className="flex w-1/4 border-r border-white items-center justify-center cursor-pointer" >
-                <Link href={`/bracket/${bracketId}`}>
-                    <h2 className="underline cursor-pointer text-extrabold hover:text-black">
-                        {bracketId}
-                    </h2>
-                </Link>
+            <div className="flex w-full">
+                <div className="m-auto font-bold cursor-pointer hover:underline">
+                    <Link href={`/bracket/${bracketId}`}>
+                        View
+                    </Link>
                 </div>
 
-                <div className="flex w-1/4 border-r border-white items-center justify-center" >
-                    <h2> 
-                        {name}
-                    </h2>
+                <div className="flex mx-auto mt-2 h-20 bg-purple-900 rounded-lg w-[90%]" >
+                    <div className="flex w-1/4 border-r border-white items-center justify-center" >
+                        <h2>
+                            {bracketId}
+                        </h2>
+                    </div>
+                    <div className="flex w-1/4 border-r border-white items-center justify-center" >
+                        <h2> 
+                            {name}
+                        </h2>
+                    </div>
+                    <div className="flex w-1/4 border-r border-white items-center justify-center">
+                        <h2>
+                            {total_points}
+                        </h2>
+                    </div>
+                    <div className="flex w-1/4 items-center justify-center">
+                        <h2>
+                            {ranking}
+                        </h2>
+                    </div>
                 </div>
-                <div className="flex w-1/4 border-r border-white items-center justify-center">
-                    <h2>
-                        {total_points}
-                    </h2>
-                </div>
-                <div className="flex w-1/4 items-center justify-center">
-                    <h2>
-                        {ranking}
-                    </h2>
+
+                <div className="m-auto font-bold cursor-pointer hover:underline"
+                    onClick={handleToggle}>
+                    Details
                 </div>
             </div>
+
             <div className="flex justify-center flex-wrap">
                 {showDetails && (
                     bracketInfoData?.map((item, index) => (
