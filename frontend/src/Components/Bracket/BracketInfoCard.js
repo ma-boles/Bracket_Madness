102f42f4a94682671ee5ee8e5ee673c44daafd84
ma-boles <ma_boles@hotmail.com>
2025-05-15 18:08:14 -0700

    git commit -m "updated props management"

--- frontend/src/Components/Bracket/BracketInfoCard.js
+++ frontend/src/Components/Bracket/BracketInfoCard.js
@@ -2,20 +2,26 @@ import React from "react";
 import AccuracyChart from "./AccuracyChart";
 import BarChart from "./BarChart";
 
-export default function BracketInfoCard({ round, round_rank, round_points, total_predictions, correct_predictions }) {
-    
-    const labels = ['Round 1', 'Round 2', 'Sweet 16', 'Elite 8', 'Final 4', 'Finals', 'Champion'];
-    const points = [10, 20, 15, 30, 25, 35, 40]
-    
+export default function BracketInfoCard({ bracketInfoData }) {
+
+    const filtered = bracketInfoData.filter(item => item.round && typeof item.round_points !== 'undefined' );
+
+    const labels = filtered.map(item => item.round);
+    const points = filtered.map(item => item.round_points);
+
+    const accuracyItem = bracketInfoData.find(item => item.correct_predictions !== undefined );
+    const correct = accuracyItem?.correct_predictions || 0;
+    const total = accuracyItem?.total_predictions || 0;
+
     return(
     <> 
     <div className="w-full h-96 m-2 bg-black/50 rounded-lg ">
-        <div className="flex py-1 justify-center">
-            <p className="py-1 px-2 text-xl">Correct picks: {correct_predictions} / {total_predictions}</p>
+        <div className="flex pt-2 justify-center">
+            <p className="px-2 text-xl">Picks: {correct} / {total}</p>
         </div>
         <div className="flex justify-between">
             <div className="flex items-center justify-center w-1/2">
-                <AccuracyChart correct={correct_predictions} total={total_predictions}/>
+                <AccuracyChart correct={correct} total={total}/>
             </div>
             <div className="w-1/2">
                 <BarChart labels={labels} data={points}/>
