aab22287877af8b4d809c9cdf23917107f837898
ma-boles <ma_boles@hotmail.com>
2025-05-16 18:44:53 -0700

    git commit-m "added header with logo"

--- frontend/src/app/Pool/page.js
+++ frontend/src/app/Pool/page.js
@@ -1,4 +1,5 @@
 import NavBar from "@/Components/NavBar";
+import Image from "next/image";
 import SlideCarousel from "@/Components/Pool/SlideCarousel";
 
 export default function Pool () {
@@ -7,9 +8,16 @@ export default function Pool () {
         <nav>
             <NavBar />
         </nav>
-        <div className="h-40 m-2 bg-orange-600">
-            <h1>Pool Header</h1>
-        </div>
+            <div className="flex justify-between overflow-hidden mx-2 my-8 h-50 bg-gradient-to-r from-blue-600 via-black to-black rounded-lg">
+                <h1 className="p-6 text-4xl font-bold">Pool</h1>
+                    <Image
+                        src="/BracketMadness_background.jpg"
+                        alt="header"
+                        width={570}
+                        height={330}
+                         className="py-2 object-cover opacity-40"
+                    />
+                </div>
 
         <main>
             <SlideCarousel />
