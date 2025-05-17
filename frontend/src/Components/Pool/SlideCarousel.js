7908ae63d7173ddd8b7953fb5e4cf44092d5774a
ma-boles <ma_boles@hotmail.com>
2025-05-16 18:43:38 -0700

    git commit -m "added conditional rendering of buttons"

--- frontend/src/Components/Pool/SlideCarousel.js
+++ frontend/src/Components/Pool/SlideCarousel.js
@@ -17,7 +17,7 @@ export default function SlideCarousel () {
 
     return (
         <>
-            <div className="relative overflow-hidden w-screen h-screen">
+            <div className="relative overflow-hidden w-screen h-screen bg-white/5">
 
                 {/* Slide track */}
                 <div className="flex w-[300vw] transition-transform duration-500 ease-in-out"
@@ -37,52 +37,33 @@ export default function SlideCarousel () {
 
                 {/* Navigation */}
                 {slideIndex === 1 ? (
-                    <div>
+                    <div className="text-center">
                     <button 
-                    className="absolute left-4 top-1/2 transform -translate-y-1/2 
-                    bg-blue-600 border-2 border-white font-bold w-30 p-3 rounded-4xl z-10 cursor-pointer"
+                    className="absolute left-0 top-1/2 transform -translate-y-1/2 
+                    bg-white/5 font-bold w-30 h-screen p-3 z-10 cursor-pointer
+                    hover:bg-white/10"
                     onClick={handlePrev}>
                         Create
                     </button>
                     <button 
-                    className="absolute right-4 top-1/2 transform -translate-y-1/2
-                     bg-blue-600 border-2 border-white font-bold w-30 p-3 rounded-4xl z-10 cursor-pointer"
+                    className="absolute right-0 top-1/2 transform -translate-y-1/2
+                    bg-white/5 font-bold w-30 h-screen p-3 z-10 cursor-pointer
+                    hover:bg-white/10"
                     onClick={handleNext}>
-                        Join
+                        Join 
                     </button>
                     </div>
                 ): (
                     <button 
                     onClick={() => setSlideIndex(1)}
-                    className={`absolute right-4 top-1/2 transform -translate-y-1/2 bg-blue-600 border-2 border-white font-bold w-30 p-3 rounded-4xl z-10 cursor-pointer 
-                    ${slideIndex === 0 ? 'right-4' : 'left-4' }`}>
+                    className={`absolute right-0 top-1/2 transform -translate-y-1/2 bg-white/5 font-bold w-30 h-screen p-3 z-10 cursor-pointer 
+                        hover:bg-white/10
+                    ${slideIndex === 0 ? 'right-0' : 'left-0' }`}>
                         Back
                     </button>
                 )}
-
-                {/* {slideIndex < 2 && (
-                    
-                )} */}
 
             </div>
         </>
     )
 }
-
-// {slideIndex > 0 && (
-//                     <button 
-//                     className="absolute left-4 top-1/2 transform -translate-y-1/2 
-//                     bg-blue-600 border-2 border-white font-bold w-30 p-3 rounded-4xl z-10 cursor-pointer"
-//                     onClick={handlePrev}>
-//                         Create
-//                     </button>
-//                 )}
-
-//                 {slideIndex < 2 && (
-//                     <button 
-//                     className="absolute right-4 top-1/2 transform -translate-y-1/2
-//                      bg-blue-600 border-2 border-white font-bold w-30 p-3 rounded-4xl z-10 cursor-pointer"
-//                     onClick={handleNext}>
-//                         Join
-//                     </button>
-//                 )}
