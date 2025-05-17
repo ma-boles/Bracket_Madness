8d49f7d185929e4bab3d63e0b1829018b4bc5e1e
ma-boles <ma_boles@hotmail.com>
2025-05-16 18:42:08 -0700

    git commit -m "added text"

--- frontend/src/Components/Pool/MainSlide.js
+++ frontend/src/Components/Pool/MainSlide.js
@@ -2,9 +2,19 @@ import React from "react";
 
 export default function MainSlide () {
     return (
-        <div className="bg-black/10">
-            <h1 className="text-8xl"><span className="text-yellow-500">Create</span> a pool?</h1>
-            <h1 className="text-8xl"> <span className="text-yellow-600">Join</span> a pool?</h1>
+        <div className="p-8 w-2/3 bg-black/20 items-center justify-center flex rounded-xl">
+            <div className="font-bold">
+                <h1 className="text-6xl leading-tight">Start a <span className="text-yellow-400">Bracket Battle </span>with
+                <br></br> Your Friends</h1>
+            </div>
+            <br></br>
+            <div className="w-2/3 leading-relaxed">
+                <h1 className="text-3xl">Create you own <strong>private pool, invite the crew,</strong> and <strong>compete</strong> for ultimate <strong className="text-orange-400 "> bragging rights</strong>.</h1>
+                <br></br>
+                <h1 className="text-3xl"><strong>Office rivalries, family fueds, fantasy league showdowns</strong> -- all settled right here.</h1>
+                <br></br>
+                <h1 className="text-3xl">No pressure, just pure <strong className="text-yellow-400 border"> madness</strong>  -- your way.</h1>
+            </div>
 
         </div>
     )
