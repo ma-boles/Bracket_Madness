59ed1936c5b2f16a3ca3a9373cdd77e6b6c04997
ma-boles <ma_boles@hotmail.com>
2025-05-19 18:21:43 -0700

    git commit -m "imported in SuccessModal"

--- frontend/src/Components/Pool/CreateSlide.js
+++ frontend/src/Components/Pool/CreateSlide.js
@@ -1,10 +1,28 @@
-import React from "react";
+'use client'
+import React, { useState } from "react";
+import { useRouter } from "next/navigation";
 import CreateForm from "../forms/CreateForm";
+import SuccessModal from "./SuccessModal";
 
 export default function CreateSlide () {
+        const [modalOpen, setModalOpen] = useState(true);
+        const [createdPool, setCreatedPool] = useState({ name: "", code: ""});
+        const router = useRouter();
+        
         return (
                 <div className="py-18 px-12 bg-black/60 rounded-xl">
-                    <CreateForm />
+                    <CreateForm 
+                        onSuccess={(pool) => {
+                                setCreatedPool(pool)
+                                setModalOpen(true);
+                        }}/>
+
+                    <SuccessModal 
+                        isOpen={modalOpen}
+                        onClose={() => setModalOpen(false)}
+                        poolName={createdPool.name}
+                        inviteCode={createdPool.code}
+                        onRedirect={() => router.push('/admin/dashboard')}/>
                 </div>
         )
 }
