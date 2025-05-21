eba736aa791820963a2953081646c838314439c9
ma-boles <ma_boles@hotmail.com>
2025-05-20 21:18:07 -0700

    git commit -m "removed unused code and updated styling"

--- frontend/src/Components/Pool/SuccessModal.js
+++ frontend/src/Components/Pool/SuccessModal.js
@@ -1,7 +1,7 @@
-import React from "react";
+'use client'
+import React, { useEffect } from "react";
 
-export default function SuccessModal ({ isOpen, onClose, poolName, inviteCode, onRedirect}) {
-    if(!isOpen) return null;
+export default function SuccessModal ({ onClose, poolName, inviteCode, onRedirect}) {
 
     useEffect(() => {
         const handleEscape = (e) => {
@@ -11,7 +11,6 @@ export default function SuccessModal ({ isOpen, onClose, poolName, inviteCode, o
         return () => document.removeEventListener("keydown", handleEscape);
     }, [onClose]);
 
-    if(!isOpen) return null;
 
     const handleCopy = () => {
         navigator.clipboard.writeText(inviteCode);
@@ -19,17 +18,17 @@ export default function SuccessModal ({ isOpen, onClose, poolName, inviteCode, o
 
     return(
         <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
-            <div className="bg-white p-6 rounded-2xl shadow-xl w-[90%] max-w-md text-center">
+            <div className="bg-white/10 border border-white p-6 rounded-2xl shadow-xl w-[90%] max-w-md text-center">
             <h2 className="text-2xl font-bold text-yellow-400 mb-4">Pool Created Successfully!</h2>
-                <p className="mb-2 text-lg font-medium">Pool Name:</p>
-                <p className="mb-4 text-gray-800">{poolName}</p>
+                <p className="mb-2 text-lg text-white/90 font-medium">Pool Name:</p>
+                <p className="mb-4 text-xl font-bold">{poolName}</p>
 
                 <p className="mb-2 text-lg font-medium">Invite Code:</p>
                 <div className="flex items-center justify-center gap-2 mb-6">
-                    <span className="text-blue-600 font-mono bg-gray-100 px-3 py-1 rounded">{inviteCode}</span>
+                    <span className="text-blue-600 font-mono bg-gray-100 px-3 py-1 text-xl rounded">{inviteCode}</span>
                      <button
                         onClick={handleCopy}
-                        className="text-sm px-3 py-1 bg-blue-500 text-white rounded hover:bg-blue-600"
+                        className="text-sm px-3 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
                     >
                         Copy
                      </button>
@@ -43,7 +42,7 @@ export default function SuccessModal ({ isOpen, onClose, poolName, inviteCode, o
                 </button>
                 <button
                     onClick={onClose}
-                    className="w-full text-gray-500 hover:text-gray-700 text-sm"
+                    className="w-full text-gray-300 hover:text-gray-600 text-sm"
                     >
                     Close
                 </button>
