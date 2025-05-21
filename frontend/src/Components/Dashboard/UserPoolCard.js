df0239c6ebdd33bff100abca48dea9b7f951c42f
ma-boles <ma_boles@hotmail.com>
2025-05-21 14:02:10 -0700

    git commit -m "initial commit"

---
+++ frontend/src/Components/Dashboard/UserPoolCard.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function UserPoolCard () {
+    return(
+        <div className="w-80 h-80 my-2 border-4 border-white/70 flex flex-col justify-between rounded-xl"> 
+           <div className="p-2 bg-blue-600 rounded-t-lg">
+                <h1 className="font-bold">Pool Name</h1>
+                <h1>Pool ID: #000</h1>
+           </div>
+           <div className="p-4">
+                <div className="bg-white/5 h-40">
+                    <h2 className="mb-2 underline text-lg text-center">Standings</h2>
+                    <p className="text-lg indent-3">rank - username</p>
+                </div>
+                <button className="py-2 my-2 bg-red-500 w-full cursor-pointer rounded-lg font-semibold hover:bg-red-600">LEAVE POOL</button>
+           </div>
+        </div>
+    )
+}
