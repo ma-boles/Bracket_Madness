31c2e6cdd1aebb5c9f3d1181148aa9ca8a28b419
ma-boles <ma_boles@hotmail.com>
2025-05-21 13:51:30 -0700

    git commit -m "removed Back button"

--- frontend/src/Components/Dashboard/ManagePools.js
+++ frontend/src/Components/Dashboard/ManagePools.js
@@ -18,8 +18,7 @@ export default function ManagePools() {
 
     return(
         <div className="m-2 px-4 py-2 bg-black/10 rounded-xl">
-            <div className="flex justify-between m-2 p-2">
-                <button className="underline">Back</button>
+            <div className="flex justify-end mb-2">
                 <div>
                     <button 
                     className="px-4 py-2 w-30 bg-black/20 border border-white hover:bg-white/5 cursor-pointer rounded-l-lg"
