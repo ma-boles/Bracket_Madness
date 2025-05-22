b1ab1fe9652a024cb329c4f8523a00b448c35a17
ma-boles <ma_boles@hotmail.com>
2025-05-21 22:33:12 -0700

    git comit -m "updated PoolCard component names"

--- frontend/src/Components/Dashboard/AdminView.js
+++ frontend/src/Components/Dashboard/AdminView.js
@@ -1,11 +1,11 @@
 import React from "react";
-import PoolCard from "./PoolCard";
+import AdminPoolCard from "./AdminPoolCard";
 
 export default function AdminView () {
     return (
         <div className="p-2">
             <div className="flex flex-wrap">
-                <PoolCard />
+                <AdminPoolCard />
             </div>
         </div>
     )
