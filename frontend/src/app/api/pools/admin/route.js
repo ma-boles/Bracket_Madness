6dcfca0361bbb0286293dce8fb7163fedc49b7c8
ma-boles <ma_boles@hotmail.com>
2025-05-22 15:59:53 -0700

    git commit -m "initial commit"

---
+++ frontend/src/app/api/pools/admin/route.js
@@ -0,0 +1,45 @@
+import { connectionToDatabase } from "@/db/db";
+import { NextResponse } from 'next/server';
+import { verifyToken } from "@/lib/auth";
+
+export async function GET(req) {
+  let db;
+
+  try {
+    console.log('Incoming GET request to /api/brackets');
+
+    db = await connectionToDatabase();
+    console.log('✅ Database connection established');
+
+    const token = req.cookies.get('token')?.value;
+    console.log('🔑 Token retrieved:', token ? '[REDACTED]' : 'None');
+
+    const decodedUser = verifyToken(token);
+
+    if(!decodedUser) {
+      console.warn('⛔ Invalid or missing token — unauthorized access attempt');
+
+      return NextResponse.json({ message: 'Unauthorized'}, { status: 401 });
+    }
+
+    const userId = decodedUser.userId;
+    console.log(`👤 Decoded user ID: ${userId}`);
+   
+    // Fetch all pools created by this user
+    const [rows] = await db.execute(
+        `SELECT id,
+            pool_name,
+            code
+        FROM pools
+        WHERE created_by = ?`,
+        [userId]
+    );
+
+    console.log(`Fetched ${rows.length} pools`);
+
+    return NextResponse.json(rows, { status: 200 });
+    } catch (error) {
+        console.error('Error in GET /api/pools/admin:', error)
+        return NextResponse.json({ message: 'Internal Server Error '}, { status: 500 });
+    }
+}
