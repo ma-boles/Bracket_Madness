1a11ce7482de4e05b4e51174e8882362ccfb7a39
ma-boles <ma_boles@hotmail.com>
2025-05-22 18:31:07 -0700

    git commit -m "added fetch pools function"

--- frontend/src/Components/Dashboard/AdminView.js
+++ frontend/src/Components/Dashboard/AdminView.js
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import AdminPoolCard from "./AdminPoolCard";
 
 export default function AdminView () {
+    const [pools, setPools] = useState([]);
+
+    useEffect(() => {
+        const fetchPools  = async () => {
+            const res = await fetch('/api/pools/admin');
+            const data = await res.json();
+            setPools(data.pools);
+        };
+        fetchPools();
+    }, []);
+
     return (
         <div className="p-2">
             <div className="flex flex-wrap">
-                <AdminPoolCard />
+                {pools?.map((pool) => (
+                    <AdminPoolCard 
+                        key={pool.id}
+                        poolId={pool.id}
+                        poolName={pool.pool_name}
+                        inviteCode={pool.code}/>
+
+                ))}
             </div>
         </div>
     )
