f9b7064be24ddb7bfe9d7024f09096aaafa0c72b
ma-boles <ma_boles@hotmail.com>
2025-05-22 18:29:03 -0700

    git commit -m "added fetch invites, accept invite functions"

--- frontend/src/Components/Dashboard/UserView.js
+++ frontend/src/Components/Dashboard/UserView.js
@@ -1,17 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PoolInvite from "./PoolInvite";
 import UserPoolCard from "./UserPoolCard";
 
 export default function UserView () {
+    const [invites, setInvites] = useState([]);
+
+    useEffect(() => {
+        const fetchInvites = async () => {
+            const res = await fetch('/api/invites');
+            const data = await res.json();
+            setInvites(data.invites);
+        };
+        fetchInvites();
+    }, []);
+
+    const handleAcceptInvite = async (poolId) => {
+        const res = await fetch('/api/invite/accept', {
+            method: 'POST',
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ poolId }),
+        });
+
+        if(res.ok) {
+            setInvites((prev) => prev.filter((inv) => inv.pool_id !== poolId ));
+        }
+    };
+
     return (
         <div className="w-full">
             <div className="border-4 p-2 border-white/30 rounded-lg">
                 <div className="mx-2">
                     <h2 className="font-bold text-lg">Notifications Center</h2>
                 </div>
+
                 <div className="flex p-2 justify-center items-center">
-                    <PoolInvite />
+                    {invites.map((invite) => (
+                        <PoolInvite 
+                            key={invite.pool_id}
+                            inviterName={invite.inviter_name}
+                            poolName={invite.pool_name}
+                            poolId={invite.pool_id}
+                            onAccept={handleAcceptInvite}/>
+
+                    ))}
                 </div>
+
             </div>
             <UserPoolCard />
         </div>
