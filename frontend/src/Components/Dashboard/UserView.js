11c9e0e32b24499d6d9dbb9044526812ce10eba2
ma-boles <ma_boles@hotmail.com>
2025-05-23 18:18:49 -0700

    git comit -m "added fetchPools and mapping for UserPoolCard"

--- frontend/src/Components/Dashboard/UserView.js
+++ frontend/src/Components/Dashboard/UserView.js
@@ -4,6 +4,8 @@ import UserPoolCard from "./UserPoolCard";
 
 export default function UserView () {
     const [invites, setInvites] = useState([]);
+    const [pools, setPools] = useState([]);
+
 
     useEffect(() => {
         const fetchInvites = async () => {
@@ -15,7 +17,7 @@ export default function UserView () {
     }, []);
 
     const handleAcceptInvite = async (poolId) => {
-        const res = await fetch('/api/invite/accept', {
+        const res = await fetch('/api/pools/invite/accept', {
             method: 'POST',
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ poolId }),
@@ -25,6 +27,15 @@ export default function UserView () {
             setInvites((prev) => prev.filter((inv) => inv.pool_id !== poolId ));
         }
     };
+
+    useEffect(() => {
+            const fetchPools  = async () => {
+                const res = await fetch('/api/pools/member');
+                const data = await res.json();
+                setPools(data.pools);
+            };
+            fetchPools();
+        }, []);
 
     return (
         <div className="w-full">
@@ -41,12 +52,20 @@ export default function UserView () {
                             poolName={invite.pool_name}
                             poolId={invite.pool_id}
                             onAccept={handleAcceptInvite}/>
-
                     ))}
                 </div>
 
             </div>
-            <UserPoolCard />
+
+            <div className="flex mt-2 flex-wrap">
+                {pools.map((pool) => (
+                    <UserPoolCard 
+                        key={pool.id}
+                        poolName={pool.pool_name}
+                        poolId={pool.id}
+                    />
+                ))}
+            </div>
         </div>
 
     )
