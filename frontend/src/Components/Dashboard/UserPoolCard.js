85239537da5a3b189ab3b939cb5ae77c89087be2
ma-boles <ma_boles@hotmail.com>
2025-05-23 18:19:36 -0700

    git commit -m "passing props to display pool data"

--- frontend/src/Components/Dashboard/UserPoolCard.js
+++ frontend/src/Components/Dashboard/UserPoolCard.js
@@ -1,18 +1,19 @@
 import React from "react";
 
-export default function UserPoolCard () {
+export default function UserPoolCard ({ poolId, poolName }) {
+
     return(
-        <div className="w-80 h-80 my-2 border-4 border-white/70 flex flex-col justify-between rounded-xl"> 
-           <div className="p-2 bg-blue-600 rounded-t-lg">
-                <h1 className="font-bold">Pool Name</h1>
-                <h1>Pool ID: #000</h1>
+        <div className="w-80 h-80 my-2 mx-1 border-2 border-white/70 flex flex-col justify-between rounded-xl"> 
+           <div className="p-2 bg-blue-600 rounded-t-xl">
+                <h1 className="font-bold">{poolName}</h1>
+                <h1><strong>ID</strong>: {poolId}</h1>
            </div>
            <div className="p-4">
                 <div className="bg-white/5 h-40">
                     <h2 className="mb-2 underline text-lg text-center">Standings</h2>
                     <p className="text-lg indent-3">rank - username</p>
                 </div>
-                <button className="py-2 my-2 bg-red-500 w-full cursor-pointer rounded-lg font-semibold hover:bg-red-600">LEAVE POOL</button>
+                <button className="py-2 my-2 bg-red-600 w-full rounded-lg font-semibold hover:bg-white/80 hover:text-black">LEAVE POOL</button>
            </div>
         </div>
     )
