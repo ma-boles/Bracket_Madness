b1c78b942c08b3dcc2b6be5bdd71040822a3f1da
ma-boles <ma_boles@hotmail.com>
2025-05-25 19:01:51 -0700

    git commit -m "updated for full access message"

--- frontend/src/Components/Dashboard/user/UserPoolCard.js
+++ frontend/src/Components/Dashboard/user/UserPoolCard.js
@@ -1,12 +1,12 @@
 import React from "react";
 import Link from "next/link";
 
-export default function UserPoolCard ({ poolId, poolName }) {
+export default function UserPoolCard ({ poolId, poolName, status }) {
 
     return(
         <div className="w-80 h-80 my-2 mx-1 border-2 border-white/70 flex flex-col justify-between rounded-xl"> 
            <div className="p-2 bg-blue-600 rounded-t-xl">
-                <h1 className="font-bold">{poolName}</h1>
+                <h1 className="font-bold">{poolName} {status}</h1>
                 <h1><strong>ID</strong>: {poolId}</h1>
            </div>
 
@@ -18,11 +18,12 @@ export default function UserPoolCard ({ poolId, poolName }) {
                     <button className="py-2 my-2 bg-red-600 w-full rounded-lg font-semibold hover:bg-white/80 hover:text-black">LEAVE POOL</button>
 
                     {/* if status is pending - link to fill out bracket/start building picks object with pool id*/}
-                    <div className="absolute inset-0 z-10 bg-black/50 bg-opacity-50 backdrop-blur-md rounded-xl flex items-center justify-center">
+
+                    <div className="absolute inset-0 z-10 bg-black/70 bg-opacity-50 backdrop-blur-md rounded-xl flex items-center justify-center">
                         <div className="p-8 text-center">
-                            <h2 className="p-2 text-center"><strong>Status Pending</strong> <br/> Click link to fill out a bracket.</h2>
+                            <h2 className="font-semibold text-xl mb-4">Submit a  bracket for <br></br> full access.</h2>
                             <Link href="/Submit">
-                                <button className="py-2 my-2 bg-blue-600 w-2/3 rounded-lg font-semibold hover:text-black hover:bg-white">To Bracket</button>
+                                <button className="mt-2 px-4 py-2 w-full font-medium bg-blue-600 rounded-lg hover:bg-white/80 hover:text-black">Submit Bracket</button>
                             </Link>
                         </div>
                     </div>
