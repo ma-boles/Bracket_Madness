2380ea498b31be00bcdec25f2c40304a757c79a4
ma-boles <ma_boles@hotmail.com>
2025-05-28 10:45:45 -0700

    git commit -m "addded conditional rendering to submit bracket button based on bracket_submitted"

--- frontend/src/Components/Dashboard/user/UserPoolCard.js
+++ frontend/src/Components/Dashboard/user/UserPoolCard.js
@@ -1,12 +1,12 @@
 import React from "react";
 import Link from "next/link";
 
-export default function UserPoolCard ({ poolId, poolName, status }) {
+export default function UserPoolCard ({ poolId, poolName, status, bracketSubmitted }) {
 
     return(
         <div className="w-80 h-80 my-2 mx-1 border-2 border-white/70 flex flex-col justify-between rounded-xl"> 
            <div className="p-2 bg-blue-600 rounded-t-xl">
-                <h1 className="font-bold">{poolName} {status}</h1>
+                <h1 className="font-bold">{poolName} {status} </h1>
                 <h1><strong>ID</strong>: {poolId}</h1>
            </div>
 
@@ -17,7 +17,7 @@ export default function UserPoolCard ({ poolId, poolName, status }) {
                     </div>
                     <button className="py-2 my-2 bg-red-600 w-full rounded-lg font-semibold hover:bg-white/80 hover:text-black">LEAVE POOL</button>
 
-                    {status === 'pending' && (
+                    {bracketSubmitted === 0 && (
                         <div className="absolute inset-0 z-10 bg-black/70 bg-opacity-50 backdrop-blur-md rounded-xl flex items-center justify-center">
                             <div className="p-8 text-center">
                                 <h2 className="font-semibold text-xl mb-4">Submit a  bracket for <br></br> full access.</h2>
