c6a14cfd2356c86ac18b4989a4cbe5b1e4433917
ma-boles <ma_boles@hotmail.com>
2025-06-17 16:48:15 -0700

    git commit -m "added MobileSpokane1 components"

--- frontend/src/Components/MobileBracket/RegionTabs/MobileRegion_Spokane1.js
+++ frontend/src/Components/MobileBracket/RegionTabs/MobileRegion_Spokane1.js
@@ -1,7 +1,16 @@
+import MobileSpokane1_EliteEight from "../MobileSubmit/Mobile_Spokane1/MobileSpokane1_EliteEight"
+import MobileSpokane1_FirstRound from "../MobileSubmit/Mobile_Spokane1/MobileSpokane1_FirstRound"
+import MobileSpokane1_SecondRound from "../MobileSubmit/Mobile_Spokane1/MobileSpokane1_SecondRound"
+import MobileSpokane1_SweetSixteen from "../MobileSubmit/Mobile_Spokane1/MobileSpokane1_SweetSixteen"
+
 export default function MobileRegion_Spokane1 () {
     return (
     <>
-    <h1>Spokane 1</h1>
+        <MobileSpokane1_FirstRound />
+        <MobileSpokane1_SecondRound />
+        <MobileSpokane1_SweetSixteen />
+        <MobileSpokane1_EliteEight />
+
     </>
     )
 }
