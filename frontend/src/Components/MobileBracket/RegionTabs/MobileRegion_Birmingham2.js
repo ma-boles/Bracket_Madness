7120e29befd9b5b3afa5312d0cfcc9b608b8439b
ma-boles <ma_boles@hotmail.com>
2025-06-18 14:22:06 -0700

    git commit -m "added round region compnonents"

--- frontend/src/Components/MobileBracket/RegionTabs/MobileRegion_Birmingham2.js
+++ frontend/src/Components/MobileBracket/RegionTabs/MobileRegion_Birmingham2.js
@@ -1,7 +1,16 @@
+import MobileBirmingham2_FirstRound from "../MobileSubmit/Mobile_Birmingham2/MobileBirmingham2_FirstRound"
+import MobileBirmingham2_SecondRound from "../MobileSubmit/Mobile_Birmingham2/MobileBirmingham2_SecondRound"
+import MobileBirmingham2_SweetSixteen from "../MobileSubmit/Mobile_Birmingham2/MobileBirmingham2_SweetSixteen"
+import MobileBirmingham2_EliteEight from "../MobileSubmit/Mobile_Birmingham2/MobileBirmingham2_EliteEight"
+
 export default function MobileRegion_Birmingham2 () {
+
     return (
     <>
-    <h1>Birmingham 2</h1>
+        <MobileBirmingham2_FirstRound />
+        <MobileBirmingham2_SecondRound />
+        <MobileBirmingham2_SweetSixteen />
+        <MobileBirmingham2_EliteEight />
     </>
     )
 }
--- frontend/src/Components/MobileBracket/RegionTabs/MobileRegion_Spokane1.js
+++ frontend/src/Components/MobileBracket/RegionTabs/MobileRegion_Spokane1.js
@@ -1,7 +1,7 @@
-import MobileSpokane1_EliteEight from "../MobileSubmit/Mobile_Spokane1/MobileSpokane1_EliteEight"
 import MobileSpokane1_FirstRound from "../MobileSubmit/Mobile_Spokane1/MobileSpokane1_FirstRound"
 import MobileSpokane1_SecondRound from "../MobileSubmit/Mobile_Spokane1/MobileSpokane1_SecondRound"
 import MobileSpokane1_SweetSixteen from "../MobileSubmit/Mobile_Spokane1/MobileSpokane1_SweetSixteen"
+import MobileSpokane1_EliteEight from "../MobileSubmit/Mobile_Spokane1/MobileSpokane1_EliteEight"
 
 export default function MobileRegion_Spokane1 () {
     return (
