3010add9883c02044dac2dd54f32bb661c75635b
ma-boles <ma_boles@hotmail.com>
2025-06-18 18:15:33 -0700

    git commit -m "removed region round components"

--- frontend/src/Components/MobileBracket/RegionsNav.js
+++ frontend/src/Components/MobileBracket/RegionsNav.js
@@ -1,20 +1,12 @@
-import { useState } from 'react';
-import MobileRegion_Spokane1 from './RegionTabs/MobileRegion_Spokane1';
-import MobileRegion_Birmingham2 from './RegionTabs/MobileRegion_Birmingham2';
-import MobileRegion_Birmingham3 from './RegionTabs/MobileRegion_Birmingham3';
-import MobileRegion_Spokane4 from './RegionTabs/MobileRegion_Spokane4';
-
-
-export default function RegionsNav() {
-    const [activeRegion, setActiveRegion] = useState('Spokane 1');
+export default function RegionsNav({ activeRegion, setActiveRegion }) {
 
     const regions = [
-        'Spokane 1', 'Birmingham 2', 'Birmingham 3', 'Spokane 4',
+        'Spokane 1', 'Birmin... 2', 'Birmin... 3', 'Spokane 4',
     ];
 
     return (
         <div>
-            <div className="flex justify-around p-2 bg-slate-100 rounded-lg shadow-md">
+            <div className="flex justify-around p-2 mt-1 bg-slate-100 rounded-lg shadow-md">
                 {regions.map((region) => (
                     <button
                         key={region}
@@ -26,13 +18,6 @@ export default function RegionsNav() {
                         {region}
                     </button>
                 ))}
-            </div>
-
-            <div className="mt-4">
-                {activeRegion === 'Spokane 1' && <MobileRegion_Spokane1 />}
-                {activeRegion === 'Birmingham 2' && <MobileRegion_Birmingham2 />}
-                {activeRegion === 'Birmingham 3' && <MobileRegion_Birmingham3 />}
-                {activeRegion === 'Spokane 4' && <MobileRegion_Spokane4 />}
             </div>
         </div>
     );
--- frontend/src/Components/MobileBracket/RoundsNav.js
+++ frontend/src/Components/MobileBracket/RoundsNav.js
@@ -1,13 +1,4 @@
-import { useState } from 'react';
-import MobileRound_FirstFour from './RoundTabs/MobileRound_FirstFour';
-import MobileRound_FirstRound from './RoundTabs/MobileRound_FirstRound';
-import MobileRound_SecondRound from './RoundTabs/MobileRound_SecondRound';
-import MobileRound_SweetSixteen from './RoundTabs/MobileRound_SweetSixteen';
-import MobileRound_EliteEight from './RoundTabs/MobileRound_EliteEight';
-import MobileRound_Championship from './RoundTabs/MobileRound_Championship';
-
-export default function RoundsNav() {
-    const [activeTab, setActiveTab] = useState('First Four');
+export default function RoundsNav({ activeTab, setActiveTab }) {
 
     const tabs = [
         'First Four', 'Rd 1', 'Rd 2', 'Sweet 16',
@@ -28,15 +19,6 @@ export default function RoundsNav() {
                         {tab}
                     </button>
                 ))}
-            </div>
-
-            <div className="mt-4">
-                {activeTab === 'First Four' && <MobileRound_FirstFour />}
-                {activeTab === 'Rd 1' && <MobileRound_FirstRound />}
-                {activeTab === 'Rd 2' && <MobileRound_SecondRound />}
-                {activeTab === 'Sweet 16' && <MobileRound_SweetSixteen />}
-                {activeTab === 'Elite 8' && <MobileRound_EliteEight />}
-                {activeTab === "Final 4" && <MobileRound_Championship />}
             </div>
         </div>
     );
