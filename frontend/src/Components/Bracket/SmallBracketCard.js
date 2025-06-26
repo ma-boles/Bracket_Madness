09f4437320cf900cd39fa9c1fa73ad6747fcc491
ma-boles <ma_boles@hotmail.com>
2025-06-25 18:37:22 -0700

    git comit -m "updated card layout and color"

--- frontend/src/Components/Bracket/SmallBracketCard.js
+++ frontend/src/Components/Bracket/SmallBracketCard.js
@@ -4,12 +4,12 @@ import BracketInfoCard from "./BracketInfoCard";
 import Link from "next/link";
 
 
-export default function SmallBracketCard({ name, bracketId, total_points, rank, accuracy_percentage, bracketInfoData, usePoolDisplay, poolRank, poolName }) {
+export default function SmallBracketCard({ name, bracketId, total_points, rank, accuracy_percentage, bracketInfoData, usePoolDisplay, poolRank, poolName, accuracyData }) {
     const [ showDetails, setShowDetails ] = useState(false);
 
     const styleClass = usePoolDisplay
-        ? "bg-orange-400 text-black"
-        : "bg-blue-900";
+        ? "bg-yellow-400 text-black"
+        : "bg-purple-900";
 
     const displayRank = usePoolDisplay ? poolRank : rank;
     const displayName = usePoolDisplay ? poolName : name;
@@ -19,36 +19,46 @@ export default function SmallBracketCard({ name, bracketId, total_points, rank, 
     };
 
     return(
-        <div className='m-2 w-50 rounded-lg'>
+        <div className='m-2 w-full rounded-lg'>
             <div className={`border-4 border-white rounded-lg ${styleClass}`}>
-                <div className="mx-auto mt-2" >
-                    <div className="pt-2 border-b border-white items-center justify-center" >
-                        <span className="p-2 font-bold">Bracket ID | Name:</span>
-                        <h2 className="p-2">
-                            {bracketId} | {displayName}
-                        </h2>
+                <div className="mx-auto" >
+                    <div className="flex border-b border-white">
+                        <div className="w-1/2 px-2 pt-2 border-r border-white items-center justify-center" >
+                            <span className="p-2 font-bold">Bracket ID:</span>
+                            <h2 className="p-2">
+                                {bracketId}
+                            </h2>
+                        </div>
+                        <div className="w-1/2 px-2 pt-2 items-center justify-center" >
+                            <span className="p-2 font-bold">Name:</span>
+                            <h2 className="p-2">
+                                {displayName}
+                            </h2>
+                        </div>
                     </div>
-                    <div className="pt-2 border-b border-white items-center justify-center">
-                        <span className="p-2 font-bold">Points | Rank :</span>
-                        <h2 className="p-2">
-                            {total_points} | {displayRank}
-                        </h2>
+                    <div className="flex border-b border-white">
+                        <div className="w-1/2 px-2 pt-2 border-r border-white items-center justify-center">
+                            <span className="p-2 font-bold">Points:</span>
+                            <h2 className="p-2">
+                                {total_points}
+                            </h2>
+                        </div>
+                        <div className="w-1/2 px-2 pt-2 items-center justify-center">
+                            <span className="p-2 font-bold">Rank:</span>
+                            <h2 className="p-2">
+                                {displayRank}
+                            </h2>
+                        </div>
                     </div>
-                    <div className="pt-2 items-center justify-center">
-                        <span className="font-bold p-2">Percentage</span>
+                    <div className="px-2 pt-2 border-b border-white items-center justify-center">
+                        <span className="font-bold p-2">Percentage:</span>
                         <h2 className="p-2">
                             {accuracy_percentage}
                         </h2>
                     </div>
                 </div>
 
-                <div className="flex py-4 border-t border-white">
-                    <div className="m-auto font-bold cursor-pointer hover:underline">
-                        <Link href={`/bracket/${bracketId}`}>
-                            View
-                        </Link>
-                    </div>
-
+                <div className="flex py-4">
                     <div className="m-auto font-bold cursor-pointer hover:underline"
                         onClick={handleToggle}>
                         Details
@@ -61,6 +71,7 @@ export default function SmallBracketCard({ name, bracketId, total_points, rank, 
                         <BracketInfoCard 
                             bracketInfoData={bracketInfoData}
                             usePoolDisplay={usePoolDisplay}
+                            accuracyData={accuracyData}
                             />
                     // ))
                 )}
