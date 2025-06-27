fda44bb40181f12419ffdc71b23e25a50afe038f
ma-boles <ma_boles@hotmail.com>
2025-06-27 10:38:42 -0700

    git commit -m "updated layout"

--- frontend/src/Components/BracketLayout/DesktopBracket_Layout.js
+++ frontend/src/Components/BracketLayout/DesktopBracket_Layout.js
@@ -8,22 +8,32 @@ import Spokane4_Pick from "../spokane4_pick";
 export default function DesktopBracket_Layout () {
     return (
         <>
-            <div className="w-full">
-                    <FirstFour />
+        <div className="flex justify-center items-center">
+            <div className="w-screen h-screen overflow-x-auto hide-scrollbar"> 
+                <div className="px-0 py-6 gap-6"> 
+
+                    <div className="w-[400px] mx-auto mb-6">
+                        <FirstFour />
+                    </div>
+                    <div>
+                        <div className="flex">
+                            <Spokane1_Pick />
+                            <Birmingham2_Pick />
+                        </div>
+
+                        <div className="w-[400px] mx-auto mb-6">
+                            <ChampionshipPick />
+                        </div>
+                    
+                        <div className="flex">
+                            <Spokane4_Pick />
+                            <Birmingham3_Pick />
+                        </div>
+                    </div>
+
                 </div>
-                <div>
-                    <div className="flex">
-                        <Spokane1_Pick />
-                        <Birmingham2_Pick />
-                    </div>
-                        <ChampionshipPick />
-                    <div className="flex">
-                        <Spokane4_Pick />
-                        <Birmingham3_Pick />
-
-                    </div>
-
-                </div>        
+            </div>
+        </div>        
         </>
     )
 }
