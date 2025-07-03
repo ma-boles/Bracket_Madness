02ebc91ade68d2f1e63bc2fd506c9cf4643a26ac
ma-boles <ma_boles@hotmail.com>
2025-07-02 17:04:15 -0700

    git commit -m "removed header image"

--- frontend/src/app/Pool/page.js
+++ frontend/src/app/Pool/page.js
@@ -1,23 +1,17 @@
+'use client'
+import { useContext } from "react";
+import AuthContext from "@/context/AuthContext";
 import NavBar from "@/Components/NavBar";
-import Image from "next/image";
 import SlideCarousel from "@/Components/Pool/SlideCarousel";
 
 export default function Pool () {
+    
+
     return (
         <>
         <nav>
             <NavBar />
         </nav>
-            <div className="flex justify-between mx-2 my-8 h-50 bg-gradient-to-r from-blue-600 via-black to-black rounded-lg">
-                <h1 className="p-6 text-4xl font-bold">Pool</h1>
-                    <Image
-                        src="/BracketMadness_background.jpg"
-                        alt="header"
-                        width={570}
-                        height={330}
-                         className="py-2 object-cover opacity-40"
-                    />
-                </div>
 
         <main className="w-full relative overflow-x-hidden overflow-y-auto">
             <SlideCarousel />
