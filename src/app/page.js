91b8b83e10bb2520548c447eeec14e2265c50dca
Mary Boles <mboles.dev@gmail.com>
2025-09-05 21:15:51 -0700

    git commit -m "Added conditional rendering and navigation for demo mode"

--- src/app/page.js
+++ src/app/page.js
@@ -1,25 +1,30 @@
 'use client'
 import Image from "next/image";
 import Link from "next/link";
-import { useState } from "react";
+import { useState, useContext } from "react";
 import { useRouter } from "next/navigation";
+import { isDemo } from "@/config";
+import AuthContext from "../context/AuthContext";
 
 
 export default function Home() {
   const [isLoadingBracket, setIsLoadingBracket] = useState(false);
   const [isLoadingLogin, setIsLoadingLogin] = useState(false);
   const router = useRouter();
+  const { currentUser } = useContext(AuthContext);
+
+  const isUser = !!currentUser;
 
   const handleLoginClick = async (e) => {
     setIsLoadingLogin(true);
-    await router.push('/auth');
+
   };
 
   const handleBracketClick = async (e) => {
     setIsLoadingBracket(true);
-    await router.push('/Submit');
-
-    // await router.push('/Results');
+    const exploreRoute = isDemo && isUser ? '/Submit' : '/Results' 
+    
+    await router.push(exploreRoute);
   };
 
   return (
@@ -36,18 +41,17 @@ export default function Home() {
         />
           <div className="flex">
 
-          <Link href="/Learn-More" target="_blank" rel="noopener noreferrer">
+          <Link href={ isDemo ? '/Learn-More' : '/auth' } target="_blank" rel="noopener noreferrer">
               <button
                 className="rounded-full border border-solid border-white/[0.8] text-white transition-colors flex items-center justify-center hover:text-black hover:bg-[#f2f2f2] hover:border-transparent font-medium 
                 w-40 h-10 mx-2
                 sm:w-50 sm:h-12 sm:text-base"
-                // onClick={handleLoginClick}
+                onClick={handleLoginClick}
                 disabled={isLoadingLogin}>
                   {isLoadingLogin ? (
                     <div className="w-5 h-5 border-2 border-white border-t-transparent rounded-full animate-spin" />
                   ) : (
-                    'Learn More'
-                    // 'Log In / Sign Up'
+                    isDemo ? 'Learn More' : 'Log In  / Sign Up'
                   )}
               </button>
           </Link>
@@ -61,8 +65,7 @@ export default function Home() {
                 {isLoadingBracket ? (
                   <div className="w-5 h-5 border-2 border-white border-t-transparent rounded-full animate-spin" />
                 ) : (
-                  'Explore App'
-                  // 'Bracket'
+                  isDemo && isUser ? 'Explore App' : 'Bracket'
                 )}
             </button>
           </div>
