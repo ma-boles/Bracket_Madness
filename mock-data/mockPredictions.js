9b0dd7ccaf840ee62d8a929d9e3da076c0c38844
Mary Boles <mboles.dev@gmail.com>
2025-09-07 16:28:53 -0700

    git commit -m "initial commit"

---
+++ mock-data/mockPredictions.js
@@ -0,0 +1,86 @@
+export const mockPreidctions = [
+
+  /* First Four */
+  { "user_id": 1, "bracket_id": 1, "game_id": 1001, "winner_id": 16, "is_scored": true, "is_correct": false },
+  { "user_id": 1, "bracket_id": 1, "game_id": 1002, "winner_id": 29, "is_scored": true, "is_correct": true },
+  { "user_id": 1, "bracket_id": 1, "game_id": 1003, "winner_id": 46, "is_scored": true, "is_correct": false },
+  { "user_id": 1, "bracket_id": 1, "game_id": 1004, "winner_id": 52, "is_scored": true, "is_correct": true },
+
+  /* First Round */
+  { "user_id": 1, "bracket_id": 1, "game_id": 1101, "winner_id": 1, "is_scored": true, "is_correct": true },
+  { "user_id": 1, "bracket_id": 1, "game_id": 1102, "winner_id": 9, "is_scored": true, "is_correct": false },
+  { "user_id": 1, "bracket_id": 1, "game_id": 1103, "winner_id": 12, "is_scored": true, "is_correct": false },
+  { "user_id": 1, "bracket_id": 1, "game_id": 1104, "winner_id": 4, "is_scored": true, "is_correct": true },
+  { "user_id": 1, "bracket_id": 1, "game_id": 1105, "winner_id": 11, "is_scored": true, "is_correct": false },
+  { "user_id": 1, "bracket_id": 1, "game_id": 1106, "winner_id": 3, "is_scored": true, "is_correct": true },
+  { "user_id": 1, "bracket_id": 1, "game_id": 1107, "winner_id": 10, "is_scored": true, "is_correct": false },
+  { "user_id": 1, "bracket_id": 1, "game_id": 1108, "winner_id": 2, "is_scored": true, "is_correct": true },
+
+  { "user_id": 1, "bracket_id": 1, "game_id": 1109, "winner_id": 34, "is_scored": true, "is_correct": false },
+  { "user_id": 1, "bracket_id": 1, "game_id": 1110, "winner_id": 26, "is_scored": true, "is_correct": true },
+  { "user_id": 1, "bracket_id": 1, "game_id": 1111, "winner_id": 30, "is_scored": true, "is_correct": false },
+  { "user_id": 1, "bracket_id": 1, "game_id": 1112, "winner_id": 21, "is_scored": true, "is_correct": true },
+  { "user_id": 1, "bracket_id": 1, "game_id": 1113, "winner_id": 29, "is_scored": true, "is_correct": false },
+  { "user_id": 1, "bracket_id": 1, "game_id": 1114, "winner_id": 20, "is_scored": true, "is_correct": true },
+  { "user_id": 1, "bracket_id": 1, "game_id": 1115, "winner_id": 27, "is_scored": true, "is_correct": true },
+  { "user_id": 1, "bracket_id": 1, "game_id": 1116, "winner_id": 33, "is_scored": true, "is_correct": false },
+
+  { "user_id": 1, "bracket_id": 1, "game_id": 1117, "winner_id": 35, "is_scored": true, "is_correct": true },
+  { "user_id": 1, "bracket_id": 1, "game_id": 1118, "winner_id": 42, "is_scored": true, "is_correct": true },
+  { "user_id": 1, "bracket_id": 1, "game_id": 1119, "winner_id": 47, "is_scored": true, "is_correct": false },
+  { "user_id": 1, "bracket_id": 1, "game_id": 1120, "winner_id": 38, "is_scored": true, "is_correct": true },
+  { "user_id": 1, "bracket_id": 1, "game_id": 1121, "winner_id": 45, "is_scored": true, "is_correct": false },
+  { "user_id": 1, "bracket_id": 1, "game_id": 1122, "winner_id": 37, "is_scored": true, "is_correct": true },
+  { "user_id": 1, "bracket_id": 1, "game_id": 1123, "winner_id": 44, "is_scored": true, "is_correct": false },
+  { "user_id": 1, "bracket_id": 1, "game_id": 1124, "winner_id": 36, "is_scored": true, "is_correct": true },
+
+  { "user_id": 1, "bracket_id": 1, "game_id": 1125, "winner_id": 53, "is_scored": true, "is_correct": true },
+  { "user_id": 1, "bracket_id": 1, "game_id": 1126, "winner_id": 60, "is_scored": true, "is_correct": false },
+  { "user_id": 1, "bracket_id": 1, "game_id": 1127, "winner_id": 64, "is_scored": true, "is_correct": false },
+  { "user_id": 1, "bracket_id": 1, "game_id": 1128, "winner_id": 56, "is_scored": true, "is_correct": true },
+  { "user_id": 1, "bracket_id": 1, "game_id": 1129, "winner_id": 58, "is_scored": true, "is_correct": true },
+  { "user_id": 1, "bracket_id": 1, "game_id": 1130, "winner_id": 66, "is_scored": true, "is_correct": false },
+  { "user_id": 1, "bracket_id": 1, "game_id": 1131, "winner_id": 62, "is_scored": true, "is_correct": true },
+  { "user_id": 1, "bracket_id": 1, "game_id": 1132, "winner_id": 67, "is_scored": true, "is_correct": false },
+
+  /* 2nd Round */
+  { "user_id": 1, "bracket_id": 1, "game_id": 1201, "winner_id": 1, "is_scored": true, "is_correct": true },
+  { "user_id": 1, "bracket_id": 1, "game_id": 1202, "winner_id": 4, "is_scored": true, "is_correct": false },
+  { "user_id": 1, "bracket_id": 1, "game_id": 1203, "winner_id": 3, "is_scored": true, "is_correct": true },
+  { "user_id": 1, "bracket_id": 1, "game_id": 1204, "winner_id": 2, "is_scored": true, "is_correct": true },
+  { "user_id": 1, "bracket_id": 1, "game_id": 1205, "winner_id": 26, "is_scored": true, "is_correct": false },
+  { "user_id": 1, "bracket_id": 1, "game_id": 1206, "winner_id": 21, "is_scored": true, "is_correct": true },
+  { "user_id": 1, "bracket_id": 1, "game_id": 1207, "winner_id": 20, "is_scored": true, "is_correct": true },
+  { "user_id": 1, "bracket_id": 1, "game_id": 1208, "winner_id": 27, "is_scored": true, "is_correct": false },
+  { "user_id": 1, "bracket_id": 1, "game_id": 1209, "winner_id": 35, "is_scored": true, "is_correct": true },
+  { "user_id": 1, "bracket_id": 1, "game_id": 1210, "winner_id": 38, "is_scored": true, "is_correct": false },
+  { "user_id": 1, "bracket_id": 1, "game_id": 1211, "winner_id": 37, "is_scored": true, "is_correct": true },
+  { "user_id": 1, "bracket_id": 1, "game_id": 1212, "winner_id": 36, "is_scored": true, "is_correct": true },
+  { "user_id": 1, "bracket_id": 1, "game_id": 1213, "winner_id": 53, "is_scored": true, "is_correct": true },
+  { "user_id": 1, "bracket_id": 1, "game_id": 1214, "winner_id": 56, "is_scored": true, "is_correct": false },
+  { "user_id": 1, "bracket_id": 1, "game_id": 1215, "winner_id": 58, "is_scored": true, "is_correct": false },
+  { "user_id": 1, "bracket_id": 1, "game_id": 1216, "winner_id": 62, "is_scored": true, "is_correct": false },
+
+  /* Sweet 16 */
+  { "user_id": 1, "bracket_id": 1, "game_id": 1601, "winner_id": 1, "is_scored": true, "is_correct": true },
+  { "user_id": 1, "bracket_id": 1, "game_id": 1602, "winner_id": 3, "is_scored": true, "is_correct": true },
+  { "user_id": 1, "bracket_id": 1, "game_id": 1603, "winner_id": 26, "is_scored": true, "is_correct": false },
+  { "user_id": 1, "bracket_id": 1, "game_id": 1604, "winner_id": 20, "is_scored": true, "is_correct": false },
+  { "user_id": 1, "bracket_id": 1, "game_id": 1605, "winner_id": 35, "is_scored": true, "is_correct": true },
+  { "user_id": 1, "bracket_id": 1, "game_id": 1606, "winner_id": 37, "is_scored": true, "is_correct": false },
+  { "user_id": 1, "bracket_id": 1, "game_id": 1607, "winner_id": 53, "is_scored": true, "is_correct": true },
+  { "user_id": 1, "bracket_id": 1, "game_id": 1608, "winner_id": 58, "is_scored": true, "is_correct": false },
+
+  /* Elite 8 */
+  { "user_id": 1, "bracket_id": 1, "game_id": 8001, "winner_id": 1, "is_scored": true, "is_correct": true },
+  { "user_id": 1, "bracket_id": 1, "game_id": 8002, "winner_id": 20, "is_scored": true, "is_correct": false },
+  { "user_id": 1, "bracket_id": 1, "game_id": 8003, "winner_id": 37, "is_scored": true, "is_correct": false },
+  { "user_id": 1, "bracket_id": 1, "game_id": 8004, "winner_id": 53, "is_scored": true, "is_correct": false },
+
+  /* Final Four */
+  { "user_id": 1, "bracket_id": 1, "game_id": 10001, "winner_id": 1, "is_scored": true, "is_correct": false },
+  { "user_id": 1, "bracket_id": 1, "game_id": 10002, "winner_id": 20, "is_scored": true, "is_correct": false },
+
+  /* Championship */
+  {"user_id": 1, "bracket_id": 1, "game_id": 10003, "winner_id": 1, "is_scored": true, "is_correct": false }
+]
