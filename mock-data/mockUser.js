454dc02b76f23e0759834c626c1cb001599fbe21
Mary Boles <mboles.dev@gmail.com>
2025-09-07 18:41:22 -0700

    git commit -m "Added 2 new users"

--- mock-data/mockUser.js
+++ mock-data/mockUser.js
@@ -1,7 +1,23 @@
-export const mockUser = {
-  id: 1,
-  userId: 1,
-  username: 'demoUser',
-  email: 'demo@example.com',
-  created_at: '2025-09-05T12:00:00Z',
-};
+export const mockUsers = [
+  {
+    id: 1,
+    userId: 1,
+    username: 'demoUser',
+    email: 'demo@example.com',
+    created_at: '2025-09-05T12:00:00Z',
+  },
+  {
+    id: 2,
+    userId: 2,
+    username: 'secondUser',
+    email: 'seconduser@example.com',
+    created_at: '2025-09-06T15:30:00Z',
+  },
+  {
+    id: 3,
+    userId: 3,
+    username: 'thirdUser',
+    email: 'third@example.com',
+    created_at: '2025-09-07T10:15:00Z',
+  },
+];
