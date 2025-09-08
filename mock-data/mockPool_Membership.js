c94368a516a811626df449ff8c7f5184ee18dedf
Mary Boles <mboles.dev@gmail.com>
2025-09-07 18:16:45 -0700

    git commit -m "initial commit"

---
+++ mock-data/mockPool_Membership.js
@@ -0,0 +1,13 @@
+export const mockPool_Membership = [
+    {
+        "id": 1,
+        "pool_id": 1,
+        "user_id": 1,
+        "joined_at": "2025-09-07T12:30:00Z",
+        "bracket_id": 1,
+        "role": "admin",
+        "status": "active",
+        "bracket_submitted": true
+    }
+
+]
---
+++ mock-data/mockPools.js
@@ -0,0 +1,10 @@
+export const mockPools = [
+    {
+        "id": 1,
+        "pool_name": "March Madness 2025",
+        "code": "MM2025XYZ123",
+        "created_by": 1,
+        "created_at": "2025-09-07T12:00:00Z"
+    }
+
+]
