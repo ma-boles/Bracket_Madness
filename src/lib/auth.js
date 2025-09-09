cf493aed82c312cd47ecc140d9d07eab8103c7ff
Mary Boles <mboles.dev@gmail.com>
2025-09-09 16:36:11 -0700

    git commit -m "Hardcoded sign in of demoUser for demo mode"

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,6 +1,17 @@
+import { isDemo } from '@/config';
 import jwt from 'jsonwebtoken';
 
 export function verifyToken(token) {
+    if(isDemo) {
+        console.log("Demo mode - returning mock user for token");
+        return {
+                id: 1,
+                userId: 1,
+                username: 'demoUser',
+                email: 'demo@example.com',
+        };
+    }
+    
     try {
         return jwt.verify(token, process.env.JWT_SECRET);
     } catch(error) {
