945196c3e1ebcc278fe15ef6bdf2e502ff145f93
Mary Boles <mboles.dev@gmail.com>
2025-09-09 16:37:51 -0700

    git commit -m "Updated LoadingMessage import location"

--- src/app/bracket/[bracketId]/loading.js
+++ src/app/bracket/[bracketId]/loading.js
@@ -1,4 +1,4 @@
-import LoadingMessage from '@/Components/ui/LoadingMessage';
+import LoadingMessage from '@/src/Components/ui/LoadingMessage';
 
 export default function Loading() {
   return <LoadingMessage />;
