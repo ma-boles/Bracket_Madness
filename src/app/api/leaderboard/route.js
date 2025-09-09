e30b6f59b51818f24256fcadf12266cdd27a90e5
Mary Boles <mboles.dev@gmail.com>
2025-09-09 16:40:33 -0700

    git commit -m "Added getBracketRank"

--- src/app/api/leaderboard/route.js
+++ src/app/api/leaderboard/route.js
@@ -1,11 +1,11 @@
 import { NextResponse } from 'next/server';
-import { getBrackets } from "@/src/lib/brackets"
+import { getBracketRank } from '@/src/lib/bracketRank';
 
 export async function GET() {
 
     try {
-        const brackets = await getBrackets();
-        return NextResponse.json(brackets);
+        const data = await getBracketRank();
+        return NextResponse.json({ data });
     } catch(error) {
         console.error('Error fetching bracket rank data:', error)
         return NextResponse.json({ error: 'Failed to fetch bracket rank data'}, { status: 500 });
