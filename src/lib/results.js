da92904d9bb676c072ef1f870ea24ed26a4d7b7b
Mary Boles <mboles.dev@gmail.com>
2025-09-08 22:19:27 -0700

    git commit -m "Added code to simulate the join of mockTeams"

--- src/lib/results.js
+++ src/lib/results.js
@@ -4,7 +4,23 @@ import { pool } from "@/src/db/db"
 async function getResults () {
     if(isDemo()) {
         const { mockResults } = require("@/mock-data/mockResults");
-        return mockResults;
+        const { mockTeams } = require("@/mock-data/mockTeams");
+
+        const joinedResults = mockResults.map(result => {
+            const team = mockTeams.find(t => t.team_id === result.winner_id );
+
+            return {
+                region: team?.region ?? null,
+                seed: team?.seed ?? null,
+                team_name: team?.team_name ?? null,
+                round: result.round,
+                game_id: result.game_id,
+                winner_id: result.winner_id,
+
+            };
+        });
+        
+        return joinedResults;
     }
 
     const [rows] = await pool.execute(
