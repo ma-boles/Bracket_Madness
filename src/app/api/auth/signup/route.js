0f1e08f84cf8043253b02b18bcddbca1e0952378
Mary Boles <mboles.dev@gmail.com>
2025-09-15 10:34:30 -0700

    git commit -m "Updated bcrypt import location"

--- src/app/api/auth/signup/route.js
+++ src/app/api/auth/signup/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken'
 import { cookies } from "next/headers";
 import { pool } from "@/src/db/db";
