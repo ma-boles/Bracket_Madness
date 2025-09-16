a9963bcae80eb63baa3959b955d69cd0fe4d4894
Mary Boles <mboles.dev@gmail.com>
2025-09-16 13:13:11 -0700

    commit -m "Updated dotenv import syntax and added a reminder that scoring is not avaiable in demo mode"

--- scripts/syncScores.js
+++ scripts/syncScores.js
@@ -1,12 +1,18 @@
-require('dotenv').config();
+// Utility script for running the scoresController manually.
+// This can be used to sync live or past game scores.
+// Not active in demo mode.
+// -----------------------------------------------------
+
+import dotenv from "dotenv";
 const {runScoresSync} = require('../controllers/scoresController');
 
-// Live scores
-// runScoresSync( 'live', true );
+dotenv.config();
 
-// Past scores
-runScoresSync( 'past', false );
+// runScoresSync( 'live', true ); //Sync live scores
 
+// runScoresSync( 'past', false ); // Sync past scores
+
+// Default async runner:
 // (async () => {
 //     try {
 //         await runScoresSync();
